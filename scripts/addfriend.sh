93a70bb446f87aa094664896c598865c87c07a82
Mark Vayngrib <mark@tradle.io>
2017-09-28 14:26:58 -0400

    fix log in script

--- scripts/addfriend.sh
+++ scripts/addfriend.sh
@@ -19,7 +19,7 @@ done
 
 if [ -z "$stage" ] || [ -z "url" ]; then
   # stage=$(./scripts/var.js custom.stage)
-  echo '"name", "url" and "stage" are required'
+  echo '-u (url) and -s (stage) are required'
   exit 1
 fi
 
