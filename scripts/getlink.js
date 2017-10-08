709fb594fcf37f4928174ffe96426658f85e8878
Mark Vayngrib <mark@tradle.io>
2017-10-07 22:23:19 -0400

    add getlink script

---
+++ scripts/getlink.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+// get the 'link' of an object
+
+const { utils } = require('@tradle/engine')
+
+let str = ''
+process.stdin
+  .on('data', data => {
+    str += data.toString()
+  })
+  .on('error', err => {
+    throw err
+  })
+  .on('end', function () {
+    process.stdout.write(utils.hexLink(JSON.parse(str)))
+  })
