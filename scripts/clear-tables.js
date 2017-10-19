ea084a510153a8c04241b21c2e4638bf365583c9
Mark Vayngrib <mark@tradle.io>
2017-10-19 17:19:02 -0400

    skip some tables in clear-tables

--- scripts/clear-tables.js
+++ scripts/clear-tables.js
@@ -9,26 +9,44 @@ const yn = require('yn')
 const { aws, env, dbUtils } = require('../').tradle
 const { listTables, clear } = dbUtils
 const readline = require('readline')
-const rl = readline.createInterface(process.stdin, process.stdout)
+const tableToClear = process.argv.slice(2)
+const skip = [
+  'pubkeys',
+  'presence',
+  'events',
+  'seals'
+]
 
-const clearTables = co.wrap(function* () {
-  let toClear = yield listTables(env)
-  toClear = toClear.filter(name => name !== env.SERVERLESS_PREFIX + 'pubkeys')
+const { href } = aws.dynamodb.endpoint
+const getTablesToClear = co.wrap(function* (tables=process.argv.slice(2)) {
+  if (!tables.length) {
+    tables = yield listTables(env)
+    tables = tables.filter(name => {
+      return !skip.find(skippable => env.SERVERLESS_PREFIX + skippable === name)
+    })
 
-  const { href } = aws.dynamodb.endpoint
-  console.log(`will empty the following tables at endpoint ${href}\n`, toClear)
-  const answer = yield new Promise(resolve => {
-    rl.question('continue? y/[n]:', resolve)
-  })
+    console.log(`will empty the following tables at endpoint ${href}\n`, tables)
+    const rl = readline.createInterface(process.stdin, process.stdout)
+    const answer = yield new Promise(resolve => {
+      rl.question('continue? y/[n]:', resolve)
+    })
 
-  rl.close()
-  if (!yn(answer)) {
-    console.log('aborted')
-    return
+    rl.close()
+    if (!yn(answer)) {
+      console.log('aborted')
+      return
+    }
   }
 
+  return tables
+})
+
+const clearTables = co.wrap(function* () {
+  const tables = yield getTablesToClear()
+  console.log(`will empty the following tables at endpoint ${href}\n`, tables)
   console.log('let the games begin!')
-  yield toClear.map(clear)
+  yield tables.map(clear)
+  console.log('done!')
 })
 
 clearTables().catch(err => {
