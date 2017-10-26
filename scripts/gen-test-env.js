1ba601fe9dc864a34e2aa9e05dbe22d53659c9ea
Mark Vayngrib <mark@tradle.io>
2017-10-26 17:54:45 -0400

    download latest deployed cloudformation template in gen-test-env script

--- scripts/gen-test-env.js
+++ scripts/gen-test-env.js
@@ -8,21 +8,28 @@ const promisify = require('pify')
 const { exec } = promisify(require('child_process'))
 const fs = promisify(require('fs'))
 const { prettify } = require('../lib/string-utils')
-const { lambdaUtils } = require('../').tradle
+const { aws, lambdaUtils } = require('../').tradle
 const serviceMapPath = path.resolve(__dirname, '../lib/cli/remote-service-map.json')
-const { loadCredentials } = require('../lib/cli/utils')
+const latestTemplatePath = path.resolve(__dirname, '../lib/cli/cloudformation-template.json')
+const { loadCredentials, downloadDeploymentTemplate } = require('../lib/cli/utils')
 const serverlessYml = require('../lib/cli/serverless-yml')
-
-// when this is merged:
-// https://github.com/serverless/serverless/pull/4169
-// use it to interpolate variables and read serverlessYml.custom.prefix
-co(function* () {
-  const { service, custom } = serverlessYml
-  const prefix = `${service}-${custom.stage}-`
+const { service, custom } = serverlessYml
+const prefix = `${service}-${custom.stage}-`
+const getEnv = co.wrap(function* () {
   const setEnvFnName = `${prefix}onmessage`
   const { Environment } = yield lambdaUtils.getConfiguration(setEnvFnName)
   yield fs.writeFile(serviceMapPath, prettify(Environment.Variables))
 })
+
+const getTemplate = co.wrap(function* () {
+  const template = yield downloadDeploymentTemplate()
+  yield fs.writeFile(latestTemplatePath, prettify(template))
+})
+
+Promise.all([
+  getEnv(),
+  getTemplate()
+])
 .catch(err => {
   console.error(err)
   process.exit(1)
