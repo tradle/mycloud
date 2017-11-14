tradle/mycloud

commit 8c36d18046aa2df766a9e3d4a9f643ccda57bf6c

fix signing bug for objects with embedded media

lib/provider.js
@@ -52,6 +52,7 @@ class Provider {
             const key = crypto_1.getSigningKey(author.keys);
             const signed = yield crypto_1.sign({ key, object });
             this.objects.addMetadata(signed);
+            this.logger.debug(`signed`, utils_1.summarizeObject(signed));
             utils_1.setVirtual(signed, { _author: crypto_1.getPermalink(author.identity) });
             return signed;
         });
@@ -62,7 +63,7 @@ class Provider {
             if (!object[constants_1.SIG]) {
                 object = yield this.signObject({ author, object });
             }
-            yield this.objects.put(object);
+            yield this.objects.put(utils_1.deepClone(object));
             this.objects.addMetadata(object);
             return object;
         });
@@ -183,6 +184,7 @@ class Provider {
                 promisePayload,
                 promiseRecipient
             ]);
+            yield this.objects.resolveEmbeds(payload);
             const payloadVirtual = utils_1.pickVirtual(payload);
             const unsignedMessage = utils_1.clone(other, {
                 [constants_1.TYPE]: MESSAGE,
@@ -197,7 +199,6 @@ class Provider {
             while (attemptsToGo--) {
                 utils_1.extend(unsignedMessage, this.messages.getPropsDerivedFromLast(prev));
                 seq = unsignedMessage[constants_1.SEQ];
-                this.logger.debug(`signing message ${seq} to ${recipient}`);
                 signedMessage = yield this.signObject({ author, object: unsignedMessage });
                 utils_1.setVirtual(signedMessage, {
                     _author: crypto_1.getPermalink(author.identity),
@@ -212,9 +213,11 @@ class Provider {
                     if (err.code !== 'ConditionalCheckFailedException') {
                         throw err;
                     }
-                    this.logger.info(`seq ${seq} was taken by another message`);
+                    this.logger.info(`seq was taken by another message, retrying`, {
+                        seq,
+                        recipient
+                    });
                     prev = yield this.messages.getLastSeqAndLink({ recipient });
-                    this.logger.info(`retrying with seq ${seq}`);
                 }
             }
             const err = new Errors.PutFailed('failing after 3 retries');

src/objects.ts
@@ -10,6 +10,7 @@ import {
   setVirtual,
   download,
   pick,
+  summarizeObject,
   RESOLVED_PROMISE
 } from './utils'
 import { extractSigPubKey, addLinks } from './crypto'
@@ -105,7 +106,7 @@ export default class Objects {
     this.addMetadata(object)
     object = deepClone(object)
     await this.replaceEmbeds(object)
-    this.logger.debug('putting', pick(object, [TYPE, '_link']))
+    this.logger.debug('putting', summarizeObject(object))
     return this.bucket.putJSON(object._link, object)
   }
 
@@ -126,7 +127,11 @@ export default class Objects {
     Embed.presignUrls({
       object,
       sign: ({ bucket, key, path }) => {
-        this.logger.debug(`pre-signing url for ${object[TYPE]} property ${path}`)
+        this.logger.debug('pre-signing url for', {
+          type: object[TYPE],
+          property: path
+        })
+
         return this.s3Utils.createPresignedUrl({ bucket, key })
       }
     })

src/provider.ts
@@ -5,9 +5,11 @@ import {
   cachifyPromiser,
   extend,
   clone,
+  deepClone,
   setVirtual,
   pickVirtual,
-  typeforce
+  typeforce,
+  summarizeObject
 } from './utils'
 
 import * as Errors from './errors'
@@ -99,6 +101,7 @@ export default class Provider {
     const signed = await sign({ key, object })
 
     this.objects.addMetadata(signed)
+    this.logger.debug(`signed`, summarizeObject(signed))
     setVirtual(signed, { _author: getPermalink(author.identity) })
     return signed
   }
@@ -112,7 +115,7 @@ export default class Provider {
       object = await this.signObject({ author, object })
     }
 
-    await this.objects.put(object)
+    await this.objects.put(deepClone(object))
     this.objects.addMetadata(object)
     return object
   }
@@ -296,6 +299,7 @@ export default class Provider {
       promiseRecipient
     ])
 
+    await this.objects.resolveEmbeds(payload)
     const payloadVirtual = pickVirtual(payload)
     const unsignedMessage = clone(other, {
       [TYPE]: MESSAGE,
@@ -316,8 +320,6 @@ export default class Provider {
       extend(unsignedMessage, this.messages.getPropsDerivedFromLast(prev))
 
       seq = unsignedMessage[SEQ]
-      this.logger.debug(`signing message ${seq} to ${recipient}`)
-
       signedMessage = await this.signObject({ author, object: unsignedMessage })
       setVirtual(signedMessage, {
         _author: getPermalink(author.identity),
@@ -325,7 +327,6 @@ export default class Provider {
       })
 
       setVirtual(signedMessage.object, payloadVirtual)
-
       try {
         await this.messages.putMessage(signedMessage)
         return signedMessage
@@ -334,9 +335,12 @@ export default class Provider {
           throw err
         }
 
-        this.logger.info(`seq ${seq} was taken by another message`)
+        this.logger.info(`seq was taken by another message, retrying`, {
+          seq,
+          recipient
+        })
+
         prev = await this.messages.getLastSeqAndLink({ recipient })
-        this.logger.info(`retrying with seq ${seq}`)
       }
     }
 

lib/objects.js
@@ -74,7 +74,7 @@ class Objects {
             this.addMetadata(object);
             object = utils_1.deepClone(object);
             yield this.replaceEmbeds(object);
-            this.logger.debug('putting', utils_1.pick(object, [constants_1.TYPE, '_link']));
+            this.logger.debug('putting', utils_1.summarizeObject(object));
             return this.bucket.putJSON(object._link, object);
         });
         this.prefetch = (link) => {
@@ -88,7 +88,10 @@ class Objects {
             Embed.presignUrls({
                 object,
                 sign: ({ bucket, key, path }) => {
-                    this.logger.debug(`pre-signing url for ${object[constants_1.TYPE]} property ${path}`);
+                    this.logger.debug('pre-signing url for', {
+                        type: object[constants_1.TYPE],
+                        property: path
+                    });
                     return this.s3Utils.createPresignedUrl({ bucket, key });
                 }
             });

lib/samplebot/index.js
@@ -1,3 +1,11 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const shallowClone = require('xtend');
 const buildResource = require('@tradle/build-resource');
 const { IS_LAMBDA_ENVIRONMENT, NODE_ENV } = process.env;
@@ -32,7 +40,7 @@ const { bot, productsAPI, employeeManager } = strategies.products({
     autoApprove: yn(AUTO_APPROVE_APPS),
     graphqlRequiresAuth: yn(GRAPHQL_AUTH)
 });
-bot.hook('message', co(function* ({ user, type }) {
+bot.hook('message', ({ user, type }) => __awaiter(this, void 0, void 0, function* () {
     debug(`received ${type}`);
     if (type === 'tradle.Ping') {
         yield bot.send({
@@ -49,20 +57,15 @@ const cacheableConf = bot.resources.buckets.PublicConf.getCacheable({
     ttl: 60000,
     parse: JSON.parse.bind(JSON)
 });
-const getConf = co(function* () {
-    try {
-        return yield cacheableConf.get();
-    }
-    catch (err) {
-        return require('./default-conf');
-    }
+const getConf = () => __awaiter(this, void 0, void 0, function* () {
+    return require('./default-conf');
 });
-const getPluginConf = co(function* (pluginName) {
+const getPluginConf = (pluginName) => __awaiter(this, void 0, void 0, function* () {
     const conf = yield getConf();
     const { plugins = {} } = conf;
     return plugins[pluginName];
 });
-const customize = co(function* () {
+const customize = () => __awaiter(this, void 0, void 0, function* () {
     const customizeMessage = require('@tradle/plugin-customize-message');
     productsAPI.plugins.use(customizeMessage({
         get models() {

lib/utils.js
@@ -479,4 +479,12 @@ exports.networkFromIdentifier = str => {
     const forFlavor = networks[flavor] || {};
     return forFlavor[networkName];
 };
+exports.summarizeObject = object => {
+    const links = buildResource.links(object);
+    const summary = Object.assign({}, links, { type: object[TYPE] });
+    if (object[TYPE] === 'tradle.Message') {
+        summary.payload = utils.summarizeObject(object.object);
+    }
+    return summary;
+};
 //# sourceMappingURL=utils.js.map

src/samplebot/index.ts
@@ -57,10 +57,10 @@ const {
 // }
 
 // prepend this hook
-bot.hook('message', co(function* ({ user, type }) {
+bot.hook('message', async ({ user, type }) => {
   debug(`received ${type}`)
   if (type === 'tradle.Ping') {
-    yield bot.send({
+    await bot.send({
       to: user.id,
       object: {
         [TYPE]: 'tradle.Pong'
@@ -70,7 +70,7 @@ bot.hook('message', co(function* ({ user, type }) {
     // prevent further processing
     return false
   }
-}), true) // prepend
+}, true) // prepend
 
 const cacheableConf = bot.resources.buckets.PublicConf.getCacheable({
   key: 'bot-conf.json',
@@ -78,21 +78,21 @@ const cacheableConf = bot.resources.buckets.PublicConf.getCacheable({
   parse: JSON.parse.bind(JSON)
 })
 
-const getConf = co(function* () {
-  try {
-    return yield cacheableConf.get()
-  } catch (err) {
+const getConf = async () => {
+  // try {
+  //   return await cacheableConf.get()
+  // } catch (err) {
     return require('./default-conf')
-  }
-})
+  // }
+}
 
-const getPluginConf = co(function* (pluginName) {
-  const conf = yield getConf()
+const getPluginConf = async (pluginName) => {
+  const conf = await getConf()
   const { plugins={} } = conf
   return plugins[pluginName]
-})
+}
 
-const customize = co(function* () {
+const customize = async () => {
   const customizeMessage = require('@tradle/plugin-customize-message')
   productsAPI.plugins.use(customizeMessage({
     get models () {
@@ -116,14 +116,14 @@ const customize = co(function* () {
       return productsAPI.models.all
     }
   }), true))
-})
+}
 
 customize().then(() => bot.ready())
 
 // if (NODE_ENV === 'test') {
 //   const user = require('../bot/tester')({ bot })
 //   coExec(function* () {
-//     // yield user.sendSelfIntroduction()
+//     // await user.sendSelfIntroduction()
 //     debugger
 //     user.send(buildResource({
 //       models: productsAPI.models,
@@ -133,7 +133,7 @@ customize().then(() => bot.ready())
 //       }
 //     }).toJSON())
 
-//     const received = yield user.awaitMessage()
+//     const received = await user.awaitMessage()
 //     console.log(received)
 //   })
 //   .catch(console.error)

src/utils.ts
@@ -639,3 +639,17 @@ exports.networkFromIdentifier = str => {
   const forFlavor = networks[flavor] || {}
   return forFlavor[networkName]
 }
+
+exports.summarizeObject = object => {
+  const links = buildResource.links(object)
+  const summary = {
+    ...links,
+    type: object[TYPE]
+  }
+
+  if (object[TYPE] === 'tradle.Message') {
+    summary.payload = utils.summarizeObject(object.object)
+  }
+
+  return summary
+}

commit 7025084e515e7432f1086f3b475e5c6bb5486cea

fix: bug with embeds introduced in 8c36d18

lib/provider.js
@@ -8,7 +8,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotProp = require("dot-prop");
 const engine_1 = require("@tradle/engine");
+const Embed = require("@tradle/embed");
 const crypto_1 = require("./crypto");
 const utils_1 = require("./utils");
 const Errors = require("./errors");
@@ -63,7 +65,7 @@ class Provider {
             if (!object[constants_1.SIG]) {
                 object = yield this.signObject({ author, object });
             }
-            yield this.objects.put(utils_1.deepClone(object));
+            yield this.objects.put(object);
             this.objects.addMetadata(object);
             return object;
         });
@@ -184,6 +186,7 @@ class Provider {
                 promisePayload,
                 promiseRecipient
             ]);
+            const embeds = Embed.getEmbeds(payload);
             yield this.objects.resolveEmbeds(payload);
             const payloadVirtual = utils_1.pickVirtual(payload);
             const unsignedMessage = utils_1.clone(other, {
@@ -207,6 +210,9 @@ class Provider {
                 utils_1.setVirtual(signedMessage.object, payloadVirtual);
                 try {
                     yield this.messages.putMessage(signedMessage);
+                    embeds.forEach(embed => {
+                        dotProp.set(signedMessage.object, embed.path, embed.value);
+                    });
                     return signedMessage;
                 }
                 catch (err) {

src/objects.ts
@@ -1,7 +1,7 @@
-import * as Embed from '@tradle/embed'
+import Embed = require('@tradle/embed')
 import { protocol } from '@tradle/engine'
 import { IDebug, ITradleObject } from './types'
-import * as types from './typeforce-types'
+import types = require('./typeforce-types')
 import { InvalidSignature, InvalidAuthor, InvalidVersion, NotFound } from './errors'
 import { TYPE, PREVLINK, PERMALINK } from './constants'
 import {
@@ -11,7 +11,7 @@ import {
   download,
   pick,
   summarizeObject,
-  RESOLVED_PROMISE
+  RESOLVED_PROMISE,
 } from './utils'
 import { extractSigPubKey, addLinks } from './crypto'
 // const { get, put, createPresignedUrl } = require('./s3-utils')

src/provider.ts
@@ -1,11 +1,12 @@
 import Debug from 'debug'
+import dotProp = require('dot-prop')
 import { utils } from '@tradle/engine'
+import Embed = require('@tradle/embed')
 import { sign, getSigningKey, getChainKey, getPermalink } from './crypto'
 import {
   cachifyPromiser,
   extend,
   clone,
-  deepClone,
   setVirtual,
   pickVirtual,
   typeforce,
@@ -115,7 +116,7 @@ export default class Provider {
       object = await this.signObject({ author, object })
     }
 
-    await this.objects.put(deepClone(object))
+    await this.objects.put(object)
     this.objects.addMetadata(object)
     return object
   }
@@ -299,6 +300,9 @@ export default class Provider {
       promiseRecipient
     ])
 
+    const embeds = Embed.getEmbeds(payload)
+    // the signature will be validated against the embeded
+    // data url
     await this.objects.resolveEmbeds(payload)
     const payloadVirtual = pickVirtual(payload)
     const unsignedMessage = clone(other, {
@@ -329,6 +333,11 @@ export default class Provider {
       setVirtual(signedMessage.object, payloadVirtual)
       try {
         await this.messages.putMessage(signedMessage)
+        // restore embed links
+        embeds.forEach(embed => {
+          dotProp.set(signedMessage.object, embed.path, embed.value)
+        })
+
         return signedMessage
       } catch (err) {
         if (err.code !== 'ConditionalCheckFailedException') {