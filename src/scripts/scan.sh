530e9e3fa439bbb0d8e4bb76c30f0e86182a046d
Mark Vayngrib <mark@tradle.io>
2017-11-17 19:34:24 -0500

    fix: parameter validation in scripts/scan.sh

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 TABLE="$1"
+if [ -z "$TABLE" ]; then
+  echo 'expected table name as first parameter'
+  exit 1
+fi
+
 ENDPOINT="--endpoint-url http://localhost:4569"
 
 while getopts remote: opt; do
@@ -14,9 +19,9 @@ while getopts remote: opt; do
 done
 
 if [ "$ENDPOINT" == "" ]; then
-  echo "scanning remote db"
+  echo "scanning remote table: $TABLE"
 else
-  echo "scanning local db"
+  echo "scanning local table: $TABLE"
 fi
 
 eval "aws dynamodb scan --table-name $TABLE $ENDPOINT" | ./lib/scripts/unmarshal.js
