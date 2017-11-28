0698c08b4501623462c56cf5260836d2d51694b0
Mark Vayngrib <mark@tradle.io>
2017-11-28 15:33:15 -0500

    fix: simplify copy-static-assets.sh

--- src/scripts/copy-static-assets.sh
+++ src/scripts/copy-static-assets.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-cp src/*.json lib/
-cp src/cli/*.json lib/cli/
-cp src/test/*.json lib/test/
-cp -r src/test/fixtures lib/test/
-cp src/samplebot/*.json lib/samplebot/
-
-cp -r src/samplebot/conf lib/samplebot/
+cd src &&
+find . -type f \( -name "*.json" -o -name "*.js" \) -exec cp --parents \{\} ../lib \; &&
+cd ..
 
 chmod +x lib/scripts/*.js
