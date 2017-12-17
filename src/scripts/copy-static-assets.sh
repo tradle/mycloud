a6bc5ad40453afc28436d48eabea808f1152d7c0
Mark Vayngrib <mark@tradle.io>
2017-12-17 18:09:45 -0500

    fix: copy-static-assets script - use rsync instead of cp --parents due to compatibility issues

--- src/scripts/copy-static-assets.sh
+++ src/scripts/copy-static-assets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd src &&
-find . -type f \( -name "*.json" -o -name "*.js" \) -exec cp --parents \{\} ../lib \; &&
+find . -type f \( -name "*.json" -o -name "*.js" \) -exec rsync -R \{\} ../lib \; &&
 cd ..
 
 chmod +x lib/scripts/*.js
