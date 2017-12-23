9e48754945cf32f46f4f507b5e1469661bdf2b97
Mark Vayngrib <mark@tradle.io>
2017-12-23 18:14:59 -0500

    fix: rebuild:lambda

---
+++ src/scripts/filter-native-modules.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import path = require('path')
+import { getNativeModules } from '../cli/utils'
+import { uniqueStrict } from '../utils'
+
+const modules = []
+const promiseNative = getNativeModules()
+
+process.stdin
+  .on('data', paths => {
+    paths.toString().split('\n').forEach(filePath => {
+      modules.push(filePath.split('node_modules/').pop())
+    })
+  })
+  .on('end', async () => {
+    const native = await promiseNative
+    const prodNative = uniqueStrict(modules).filter(name => {
+      return native.find(str => str === name)
+    })
+
+    process.stdout.write(prodNative.join(' '))
+  })
+
+process.on('unhandledRejection', err => {
+  process.exitCode = 1
+  console.error(err.stack)
+})
--- src/scripts/rebuild-native.sh
+++ src/scripts/rebuild-native.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-NATIVE=$(./lib/scripts/get-native-prod-modules.js)
-if [ "$?" == "0" ]; then
-  set -x
-  npm rebuild $NATIVE
-fi
+NATIVE=$(npm ls --production --parseable=true --long=false --silent | node ./lib/scripts/filter-native-modules.js)
+echo "rebuilding: $NATIVE"
+npm rebuild $NATIVE
