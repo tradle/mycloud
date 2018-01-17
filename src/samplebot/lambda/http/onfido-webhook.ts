8c6d8e33597c42a1aae334489ee8c77f6a3d83f6
Mark Vayngrib <mark@tradle.io>
2018-01-17 12:23:26 -0500

    fix: onfido webhook edge case

--- src/samplebot/lambda/http/onfido-webhook.ts
+++ src/samplebot/lambda/http/onfido-webhook.ts
@@ -3,6 +3,7 @@ import cors = require('kcors')
 import { createBot } from '../../../bot'
 import { customize } from '../../customize'
 import { post } from '../../../bot/middleware/noop-route'
+import Errors = require('../../../errors')
 
 const bot = createBot({ ready: false })
 const lambda = bot.createLambda({ source: EventSource.HTTP })
@@ -12,6 +13,10 @@ lambda.use(post())
 lambda.use(cors())
 lambda.use(async (ctx) => {
   const { onfidoPlugin } = await promiseCustomize
+  if (!onfidoPlugin) {
+    throw new Errors.HttpError(404, 'not found')
+  }
+
   await onfidoPlugin.processWebhookEvent({
     req: ctx.request.req
   })
