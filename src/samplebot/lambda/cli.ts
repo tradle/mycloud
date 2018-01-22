d81723fcee092f8ee1e7795187576c5ce8377ae0
Mark Vayngrib <mark@tradle.io>
2018-01-22 13:45:58 -0500

    refactor: cli lambda in dev mode

--- src/samplebot/lambda/cli.ts
+++ src/samplebot/lambda/cli.ts
@@ -1,23 +1,10 @@
-
 import { fromCli, fromHTTP } from '../../bot/lambda'
 import { createBot } from '../../bot'
 import { customize } from '../customize'
 
-const isLocal = process.env.IS_OFFLINE
 const bot = createBot({ ready: false })
-const lambda = isLocal
-  ? fromHTTP({ bot, devModeOnly: true })
-  : fromCli({ bot })
-
+const lambda = fromCli({ bot })
 const promiseComponents = customize({ lambda, event: 'message' })
-
-if (isLocal) {
-  lambda.use(require('../../bot/middleware/body-parser').bodyParser())
-  lambda.use(async (ctx, next) => {
-    ctx.event = Object.keys(ctx.event)[0]
-    await next()
-  })
-}
 
 lambda.use(async (ctx, next) => {
   const command = ctx.event
