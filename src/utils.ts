9b798d5533cbdcdf4b01f09ed3082116108ab1c4
Mark Vayngrib <mark@tradle.io>
2018-02-13 14:42:40 -0500

    refactor: utils.parseArn

--- src/utils.ts
+++ src/utils.ts
@@ -755,11 +755,15 @@ export const race = Promise.race
 export function parseArn (arn) {
   // e.g. arn:aws:lambda:us-east-1:210041114155:function:tradle-dev-http_catchall
   const parts = arn.split(':')
+  const relativeId = parts.slice(5).join(':')
+  const idParts = relativeId.split('/')
   return {
     service: parts[2],
     region: parts[3],
     accountId: parts[4],
-    relativeId: parts.slice(4).join(':')
+    relativeId,
+    type: idParts[0],
+    id: idParts.slice(1).join('/')
   }
 }
 
