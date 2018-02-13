9617169e5358911adc97043ad1856bf436b3ea86
Mark Vayngrib <mark@tradle.io>
2018-02-12 19:22:26 -0500

    fix: /qr

--- src/samplebot/lambda/http/qr.ts
+++ src/samplebot/lambda/http/qr.ts
@@ -1,5 +1,6 @@
 import QR = require('@tradle/qr')
 import promisify = require('pify')
+import * as Koa from 'koa'
 import { createBot } from '../../../bot'
 import { EventSource } from '../../../lambda'
 
@@ -7,17 +8,29 @@ const createDataURL = promisify(QR.toDataURL)
 const bot = createBot()
 const lambda = bot.createLambda({ source: EventSource.HTTP })
 const getPermalink = bot.getMyIdentityPermalink()
-lambda.use(async (ctx, next) => {
-  const { queryParams = {} } = ctx.event
-  const { claimId } = queryParams
+const descriptions = {
+  ImportData: ({ dataHash }: any) => `scan this QR code with the Tradle app to claim the bundle with claimId: ${dataHash}`,
+  AddProvider: (data: any) => `scan this QR code with the Tradle app to add this provider to your Conversations screen`
+}
+
+lambda.use(async (ctx:Koa.Request, next) => {
+  const { query={} } = ctx
+  const { claimId } = query
   const provider = await getPermalink
   const host = bot.apiBaseUrl
-  const dataUrl = await createDataURL({
-    schema: claimId ? 'ImportData' : 'AddProvider',
-    data: { provider, host, dataHash: claimId }
-  })
+  const schema = claimId ? 'ImportData' : 'AddProvider'
+  const data = { provider, host, dataHash: claimId }
+  const dataUrl = await createDataURL({ schema, data })
+  const description = descriptions[schema](data)
 
-  ctx.body = `<html><img src="${dataUrl}" width="300" height="300" /></html>`
+  ctx.body = `
+<html>
+  <body>
+    <h1>Type: ${schema}</h1>
+    <p>${description}</p>
+    <img src="${dataUrl}" width="300" height="300" />
+  </body>
+</html>`
 })
 
 export const handler = lambda.handler
