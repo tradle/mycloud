f19d557ee844170d34d0de8e346149042c6b3863
Mark Vayngrib <mark@tradle.io>
2018-02-16 13:13:29 -0500

    refactor(dev): run redis in docker

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -2,14 +2,6 @@
 
 set -euo pipefail
 
-REDIS_CLI=$(which redis-cli)
-REDIS_SERVER=$(which redis-server)
-if [ "$REDIS_CLI" == "" ] || [ "$REDIS_SERVER" == "" ]; then
-  echo 'please install and run redis'
-  exit 1
-fi
-
-redis-cli ping >/dev/null 2>&1 || { echo 'please start redis first (run: redis-server)'; exit 1; }
 docker ps >/dev/null 2>&1 || { echo 'please start Docker first'; exit 1; }
 
 npm run localstack:start
