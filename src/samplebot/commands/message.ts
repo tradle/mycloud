800f5eb1cbd48e8f7e3f3c263ef1a3665f16184b
Mark Vayngrib <mark@tradle.io>
2018-02-24 11:36:43 -0500

    refactor: message command

--- src/samplebot/commands/message.ts
+++ src/samplebot/commands/message.ts
@@ -1,28 +1,19 @@
 import { TYPE } from '@tradle/constants'
-import parse = require('yargs-parser')
 import { ICommand } from '../types'
 
 export const command:ICommand = {
   name: 'message',
   description: 'sends a message',
   examples: [
-    '/message --to <userId> --message "hey there"'
+    '/message --to <userId> --message <messageText>'
   ],
-  parse: (argsStr:string) => {
-    const args = parse(argsStr)
+
+  exec: async ({ commander, args }) => {
     const { to, message } = args
     if (!(to && message)) {
       throw new Error('"to" and "message" are required')
     }
 
-    return {
-      to: args.to,
-      object: {
-        [TYPE]: 'tradle.SimpleMessage',
-        message: args.message
-      }
-    }
-  },
-
-  exec: ({ commander, args }) => commander.bot.send(args)
+    await commander.bot.sendSimpleMessage({ to, message })
+  }
 }
