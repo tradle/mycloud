3b80c9e9ef902ed254ffe2c12545d494fc21ab2a
Mark Vayngrib <mark@tradle.io>
2018-02-27 10:08:58 -0500

    fix: nuke script - rm stack first, then buckets

--- src/scripts/nuke.sh
+++ src/scripts/nuke.sh
@@ -71,5 +71,5 @@ echo "This will empty and delete all buckets, tables, lambdas, etc. for"
 echo "service: $SERVICE"
 echo "stage: $STAGE"
 
-ask && ./lib/scripts/delete-remote-buckets.js
 ask "delete resources stack" && sls remove --stage="$STAGE"
+ask && ./lib/scripts/delete-remote-buckets.js
