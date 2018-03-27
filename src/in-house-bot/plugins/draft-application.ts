6b6287f8415f07a43c144a8e858f86651c1b4896
Mark Vayngrib <mark@tradle.io>
2018-03-27 19:55:15 -0400

    feat: handle draft applications

---
+++ src/in-house-bot/plugins/draft-application.ts
@@ -0,0 +1,94 @@
+import { TYPE } from '@tradle/constants'
+import {
+  CreatePlugin,
+  IPBReq
+} from '../types'
+
+const EMPLOYEE_ONBOARDING = 'tradle.EmployeeOnboarding'
+const DEPLOYMENT = 'tradle.cloud.Deployment'
+const DRAFT = 'tradle.DraftApplication'
+
+export const createPlugin:CreatePlugin<void> = (components, { logger }) => {
+  const { bot, productsAPI, employeeManager } = components
+  const handleFormPrefill = async (req:IPBReq) => {
+    const { payload } = req
+    req.draftApplication = await bot.getResourceByStub(req.payload.draft)
+    logger.debug('received form prefill', {
+      draft: req.payload.draft,
+      prefill: req.payload.prefill[TYPE]
+    })
+  }
+
+  const handleProductRequest = async (req:IPBReq) => {
+    const { user, payload, message } = req
+    const { requestFor } = payload
+    req.isFromEmployee = employeeManager.isEmployee(req.user)
+    if (!req.isFromEmployee) return
+
+    if (requestFor === EMPLOYEE_ONBOARDING &&
+      message.forward) {
+      logger.warn(`refusing to allow application for employee onboarding from own employee to another organization`, {
+        employee: user.id,
+        toOrg: message.forward
+      })
+
+      await productsAPI.sendSimpleMessage({
+        req,
+        to: user,
+        message: `You're already an employee of someone else!`
+      })
+
+      return false
+    }
+
+    const { application } = req
+    if (application) {
+      debugger
+      return
+    }
+
+    if (req.message.forward) return
+
+    // HACK
+    if (requestFor === DEPLOYMENT) return
+
+    logger.debug('creating application draft, as this is an employee applying on behalf of a customer')
+    const draftApplication = bot.buildResource(DRAFT)
+      .set({
+        applicant: user.identity,
+        context: req.context,
+        request: payload,
+        requestFor,
+        formPrefills: []
+      })
+      .toJSON()
+
+    const draft = await bot.signAndSave(draftApplication)
+    req.draftApplication = draft
+
+    const draftLink = bot.appLinks.getResourceLink({
+      platform: 'web',
+      type: DRAFT,
+      link: draft._link,
+      permalink: draft._permalink
+    })
+
+    await productsAPI.sendSimpleMessage({
+      req,
+      to: req.user,
+      message: `You have created a draft application. [Edit it here](${draftLink})`
+    })
+
+    req.skipChecks = true
+
+    // prevent futher processing
+    return false
+  }
+
+  const plugin = {
+    ['onmessage:tradle.ProductRequest']: handleProductRequest,
+    ['onmessage:tradle.FormPrefill']: handleFormPrefill,
+  }
+
+  return { plugin }
+}
