2b19d9e0afb967dbb366817c517010fdc01b5ae8
Mark Vayngrib <mark@tradle.io>
2018-04-23 21:08:31 -0400

    fix: sometimes redis needs a restart or it breaks mosca

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 docker ps >/dev/null 2>&1 || { echo 'please start Docker first'; exit 1; }
 
 npm run localstack:start
+npm run fix:redis
 sleep 5
 npm run gen:localstack
 # npm run gen:localresources
-DEBUG=λ*,*tradle* serverless offline start
+DEBUG=λ*,*tradle* ./node_modules/.bin/sls offline start
