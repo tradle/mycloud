f6f869edb30046e40c8bd2620257c74595890e34
pgmemk <ellen@tradle.io>
2018-05-03 11:00:38 -0400

    feat: notify controlling person to get onboarded

--- src/in-house-bot/plugins/controllingPersonRegistration.ts
+++ src/in-house-bot/plugins/controllingPersonRegistration.ts
@@ -2,12 +2,20 @@ import fetch from 'node-fetch'
 import _ from 'lodash'
 
 import * as utils from '../utils'
-import { Bot, Logger, CreatePlugin, Applications } from '../types'
+import { Bot, Logger, CreatePlugin, Applications, IPBApp } from '../types'
+import { Remediation } from '../remediation'
 import * as Templates from '../templates'
 import Errors from '../../errors'
+import * as crypto  from '../../crypto'
+import { TYPES } from  '../constants'
+
+const { APPLICATION, IDENTITY } = TYPES
 
 const FORM_ID = 'tradle.PersonalInfo'
+const EMPLOYEE_ONBOARDING = 'tradle.EmployeeOnboarding'
+
 const ONBOARD_MESSAGE = 'Controlling person onboarding'
+
 const DEFAULT_TEMPLATE = {
   template: 'action',
   blocks: [
@@ -27,20 +35,29 @@ class ControllingPersonRegistrationAPI {
   private logger:Logger
   private org: any
   private conf: IControllingPersonConf
-  constructor({ bot, org, conf, logger }) {
+  private applications:Applications
+  private remediation: Remediation
+  constructor({ bot, org, conf, logger, applications, remediation }) {
     this.bot = bot
     this.org = org
     this.conf = conf
     this.logger = logger
+    this.applications = applications
+    this.remediation = remediation
   }
-  async _send(resource, application) {
+  async _send(resource, invite) {
     let emailAddress = resource.emailAddress
 
     let permalink = await this.bot.getPermalink()
-    let values = utils.getAppLinks({bot: this.bot, permalink})
-    _.extend(values, {name: resource.firstName, orgName: this.org.name})
-    let body = Templates.email.action(Templates.renderData(DEFAULT_TEMPLATE, values))
+    let host = this.bot.apiBaseUrl
+    let employeeOnboarding = invite.links.web
 debugger
+    let values = {
+      employeeOnboarding,
+      name: resource.firstName,
+      orgName: this.org.name
+    }
+    let body = Templates.email.action(Templates.renderData(DEFAULT_TEMPLATE, values))
     try {
       await this.bot.mailer.send({
         from: this.conf.senderEmail,
@@ -50,22 +67,39 @@ debugger
         body
       })
     } catch (err) {
+debugger
       Errors.rethrow(err, 'developer')
       this.logger.error('failed to email controlling person', err)
     }
   }
+  async _createDraftAndInvite(payload, req) {
+    let user = req.user
+    let application =  {
+      _t: APPLICATION,
+      context: crypto.randomString(32),
+      requestFor: EMPLOYEE_ONBOARDING,
+      applicant: {
+        _t: IDENTITY,
+        _permalink: payload._author,
+        _link: payload._author
+      }
+    }
+    let draftApplication = await this.applications.createApplication({user, application, req})
+    let submission = await this.applications.createApplicationSubmission({application: draftApplication, submission: payload})
+    return await this.remediation.getInviteForDraftApp({application: draftApplication})
+  }
 }
 
 export const createPlugin: CreatePlugin<void> = (components, { logger, conf }) => {
-  const { bot } = components
+  const { bot, applications, remediation } = components
+
   const orgConf = components.conf
   const { org } = orgConf
-  const cp = new ControllingPersonRegistrationAPI({ bot, conf, org, logger })
+  const cp = new ControllingPersonRegistrationAPI({ bot, conf, org, logger, applications, remediation: remediation || new Remediation(components) })
   const plugin = {
     [`onmessage:${FORM_ID}`]: async function(req) {
       const { user, application, payload } = req
       if (!application) return
-debugger
       let productId = application.requestFor
       let { products } = conf
       if (!products  ||  !products[productId]  ||  products[productId].indexOf(FORM_ID) === -1)
@@ -78,7 +112,11 @@ debugger
         if (prevR  &&  prevR.emailAddress === payload.emailAddress)
           return
       }
-      cp._send(payload, application)
+
+      let invite = await cp._createDraftAndInvite(payload, req)
+      // applications.createApplicationSubmission({application: draftApplication, submission: payload})
+
+      cp._send(payload, invite)
     }
   }
 
