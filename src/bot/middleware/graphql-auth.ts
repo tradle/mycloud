f319f4bc130aeabcedc869f26dac4d7b195dfc86
Mark Vayngrib <mark@tradle.io>
2018-05-09 13:08:20 -0400

    tmp: disable graphql auth

--- src/bot/middleware/graphql-auth.ts
+++ src/bot/middleware/graphql-auth.ts
@@ -2,12 +2,28 @@ import { utils as tradleUtils } from '@tradle/engine'
 import validateResource from '@tradle/validate-resource'
 import constants from '../../constants'
 import Errors from '../../errors'
-import { ITradleObject } from '../../types'
+import { ITradleObject, Bot, Logger } from '../../types'
 import { isPromise } from '../../utils'
 
 const { TYPE, SIG, MAX_CLOCK_DRIFT } = constants
 
-export const createHandler = ({ bot, logger }, { allowGuest, canUserRunQuery }) => {
+type GAComponents = {
+  bot: Bot
+  logger: Logger
+}
+
+type GAOpts = {
+  allowGuest?: boolean
+  canUserRunQuery: Function
+}
+
+export const createHandler = ({
+  bot,
+  logger
+}: GAComponents, {
+  allowGuest,
+  canUserRunQuery
+}: GAOpts) => {
   const { identities } = bot
   return async (ctx, next) => {
     const method = ctx.method.toLowerCase()
@@ -25,74 +41,77 @@ export const createHandler = ({ bot, logger }, { allowGuest, canUserRunQuery }) 
       return
     }
 
-    logger.debug('authenticating')
-    const sig = ctx.headers['x-tradle-sig']
-    if (!allowGuest && sig == null) {
-      ctx.status = 403
-      ctx.body = {
-        message: `expected header "x-tradle-sig"`
-      }
-
-      logger.debug('expected sig')
-      return
-    }
-
-    const queryObj:ITradleObject = {
-      [TYPE]: 'tradle.GraphQLQuery',
-      body: tradleUtils.stringify(ctx.event)
-    }
-
-    if (sig) queryObj[SIG] = sig
-
-    try {
-      validateResource.resource({
-        models: bot.models,
-        model: bot.models['tradle.GraphQLQuery'],
-        resource: queryObj
-      })
-    } catch (err) {
-      throw new Errors.InvalidInput(`invalid tradle.GraphQLQuery: ${err.message}`)
-    }
-
-    checkDrift(queryObj.time)
-
-    let { user } = ctx
-    if (sig && !user) {
-      logger.debug('looking up query author')
-      try {
-        await identities.addAuthorInfo(queryObj)
-        ctx.user = user = await bot.users.get(queryObj._author)
-      } catch (err) {
-        Errors.rethrow(err, 'system')
-        if (Errors.isNotFound(err) || Errors.matches(err, Errors.UnknownAuthor)) {
-          ctx.status = 403
-          ctx.body = {
-            message: 'not allowed'
-          }
-        } else {
-          ctx.status = 500
-          ctx.body = {
-            message: 'something went wrong'
-          }
-        }
-
-        return
-      }
-    }
-
-    let allowed = canUserRunQuery({ user, query: queryObj })
-    if (isPromise(allowed)) allowed = await allowed
-    if (!allowed) {
-      ctx.status = 403
-      ctx.body = {
-        message: 'not allowed'
-      }
-
-      return
-    }
-
-    logger.debug('allowing')
     await next()
+    return
+
+    // logger.debug('authenticating')
+    // const sig = ctx.headers['x-tradle-sig']
+    // if (!allowGuest && sig == null) {
+    //   ctx.status = 403
+    //   ctx.body = {
+    //     message: `expected header "x-tradle-sig"`
+    //   }
+
+    //   logger.debug('expected sig')
+    //   return
+    // }
+
+    // const queryObj:ITradleObject = {
+    //   [TYPE]: 'tradle.GraphQLQuery',
+    //   body: tradleUtils.stringify(ctx.event)
+    // }
+
+    // if (sig) queryObj[SIG] = sig
+
+    // try {
+    //   validateResource.resource({
+    //     models: bot.models,
+    //     model: bot.models['tradle.GraphQLQuery'],
+    //     resource: queryObj
+    //   })
+    // } catch (err) {
+    //   throw new Errors.InvalidInput(`invalid tradle.GraphQLQuery: ${err.message}`)
+    // }
+
+    // // checkDrift(queryObj.time)
+
+    // let { user } = ctx
+    // if (sig && !user) {
+    //   logger.debug('looking up query author')
+    //   try {
+    //     await identities.verifyAuthor(queryObj)
+    //     ctx.user = user = await bot.users.get(queryObj._author)
+    //   } catch (err) {
+    //     Errors.rethrow(err, 'system')
+    //     if (Errors.isNotFound(err) || Errors.matches(err, Errors.UnknownAuthor)) {
+    //       ctx.status = 403
+    //       ctx.body = {
+    //         message: 'not allowed'
+    //       }
+    //     } else {
+    //       ctx.status = 500
+    //       ctx.body = {
+    //         message: 'something went wrong'
+    //       }
+    //     }
+
+    //     return
+    //   }
+    // }
+
+    // let allowed = canUserRunQuery({ user, query: queryObj })
+    // if (isPromise(allowed)) allowed = await allowed
+    // if (!allowed) {
+    //   ctx.status = 403
+    //   ctx.body = {
+    //     message: 'not allowed'
+    //   }
+
+    //   return
+    // }
+
+    // logger.debug('allowing')
+    // await next()
   }
 }
 
