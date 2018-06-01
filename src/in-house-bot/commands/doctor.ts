7527e5a134c3e135d5563beb5768c7501c349f99
Mark Vayngrib <mark@tradle.io>
2018-06-01 12:45:54 -0400

    add addFriends to doctor script

--- src/in-house-bot/commands/doctor.ts
+++ src/in-house-bot/commands/doctor.ts
@@ -1,6 +1,9 @@
+// @ts-ignore
+import Promise from 'bluebird'
 import parse from 'yargs-parser'
 import { ICommand, Bot } from '../types'
 import { createConf } from '../configure'
+import { allSettled } from '../../utils'
 
 export const command:ICommand = {
   name: 'doctor',
@@ -12,12 +15,23 @@ export const command:ICommand = {
     const { bot } = commander
     if (!ctx.sudo) throw new Error('forbidden')
 
-    // add self
     await addSelf(bot)
+    await addFriends(bot)
   }
 }
 
 const addSelf = async (bot: Bot) => {
   const identity = await bot.identity.getPublic()
   bot.identities.addContactWithoutValidating(identity)
+}
+
+const addFriends = async (bot: Bot) => {
+  const friends = await bot.friends.list()
+  const results = await allSettled(friends.map(async (friend) => {
+    const identity = await bot.getResource(friend.identity)
+    await bot.identities.addContactWithoutValidating(identity)
+  }))
+
+  results.filter(r => r.isRejected)
+    .forEach(r => bot.logger.error(`failed to load friend identity: ${r.reason}`))
 }
