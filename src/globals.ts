00c4cde2c1e7e8309aa1e179f010360881a945c6
Mark Vayngrib <mark@tradle.io>
2018-06-03 21:57:57 -0400

    fix: replace error-ex as it confuses bluebird

--- src/globals.ts
+++ src/globals.ts
@@ -22,6 +22,19 @@ mockery.enable({
 
 console.warn('disabling "scrypt" as it is an unneeded dep (here) of ethereumjs-wallet')
 mockery.registerMock('scrypt', {})
+
+// https://github.com/Qix-/node-error-ex
+console.warn(`replacing "error-ex" as it bluebird doesn't recognize its errors as Error objects`)
+mockery.registerMock('error-ex', name => {
+  return class CustomError extends Error {
+    public name: string
+    constructor(message) {
+      super(message)
+      this.name = name
+    }
+  }
+})
+
 if (process.env.IS_OFFLINE || process.env.IS_LOCAL || process.env.NODE_ENV === 'test') {
   console.warn('disabling "aws-xray-sdk" as this is a local environment')
   mockery.registerMock('aws-xray-sdk', null)
