b58dd232b60acc28d1a7566602419fe4207cc34a
Mark Vayngrib <mark@tradle.io>
2018-06-07 13:49:15 -0400

    build: add git merge hook to run npm install if deps changed

---
+++ src/scripts/postmerge.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+check_run() {
+  echo "$changed_files" | grep --quiet "$1" && eval "$2"
+}
+
+update_deps() {
+  echo 'dependencies have changed, running "npm install"'
+  npm install
+}
+
+echo 'compiling typescript' && tsc &
+check_run npm-shrinkwrap.json "update_deps" &
+wait
