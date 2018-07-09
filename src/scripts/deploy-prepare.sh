685286a80455cd5602c343c8e8d10cedc7044398
Mark Vayngrib <mark@tradle.io>
2018-07-08 21:14:48 -0400

    fix: disallow deploy with uncommited changes

--- src/scripts/deploy-prepare.sh
+++ src/scripts/deploy-prepare.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -euo pipefail
+if [[ $(git diff --stat) != '' ]] || [[ $(git diff --staged --stat) != '' ]]; then
+  echo 'please stash or commit before deploying'
+  exit 1
+fi
+
 tsc
 npm run eslint
 npm run build:yml
