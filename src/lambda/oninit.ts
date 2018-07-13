e1e2d03935b652376f50b576f92c56f5cc65810f
Mark Vayngrib <mark@tradle.io>
2018-07-13 19:01:28 -0400

    refactor: simplify lambda/oninit

--- src/lambda/oninit.ts
+++ src/lambda/oninit.ts
@@ -3,14 +3,10 @@ import { fromCloudFormation } from '../lambda'
 
 export const createLambda = (opts) => {
   const lambda = fromCloudFormation(opts)
-  return lambda.use(createMiddleware(lambda, opts))
-}
-
-export const createMiddleware = (lambda:Lambda, opts?:any) => {
   const { bot } = lambda
-  return async (ctx, next) => {
+  return lambda.use(async (ctx, next) => {
     const { event } = ctx
     await bot.fire(`stack:${event.type}`, ctx.event)
     await next()
-  }
+  })
 }
