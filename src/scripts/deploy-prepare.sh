0679c7bf5c61893774bd1ccd1bc436b67aebaafb
Mark Vayngrib <mark@tradle.io>
2018-07-14 13:49:52 -0400

    build: clear lib/ pre-deploy

--- src/scripts/deploy-prepare.sh
+++ src/scripts/deploy-prepare.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 set -euo pipefail
-if [[ $(git diff --stat) != '' ]] || [[ $(git diff --staged --stat) != '' ]]; then
-  echo 'please stash or commit before deploying'
-  exit 1
+if [[ $ALLOW_DIRTY != "1" ]]; then
+  if [[ $(git diff --stat) != '' ]] || [[ $(git diff --staged --stat) != '' ]]; then
+    echo 'please stash or commit before deploying'
+    exit 1
+  fi
 fi
 
+npm run clean:lib
 tsc
 npm run eslint
 npm run build:yml
