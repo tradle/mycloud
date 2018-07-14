33741f5d46dbca271bc306a902bc9054fe6694b6
Mark Vayngrib <mark@tradle.io>
2018-07-13 20:31:50 -0400

    fix: cli commands registry export

--- src/in-house-bot/commands/index.ts
+++ src/in-house-bot/commands/index.ts
@@ -7,7 +7,7 @@ const commands = {}
 
 fs.readdirSync(__dirname).forEach(file => {
   if (file !== 'index.js' && file.endsWith('.js')) {
-    const command:ICommand = require(path.resolve(__dirname, file))
+    const command:ICommand = require(path.resolve(__dirname, file)).command
     const name = command.name || path.parse(file).name
     commands[name] = command
   }
