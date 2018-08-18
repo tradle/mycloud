aebc70bfb7e710e1af0f799ca07d47615330a591
Mark Vayngrib <mark@tradle.io>
2018-08-17 23:16:30 -0400

    fix: deployment pingback bot.ready timing

--- src/in-house-bot/lambda/http/deployment-pingback.ts
+++ src/in-house-bot/lambda/http/deployment-pingback.ts
@@ -1,6 +1,6 @@
 import { createBot } from '../../../'
 import { createLambda } from '../../../in-house-bot/middleware/deployment-pingback'
 
-const bot = createBot()
+const bot = createBot({ ready: false })
 const lambda = createLambda({ bot })
 export const handler = lambda.handler
