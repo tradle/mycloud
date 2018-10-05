08ca1617fccb99c072a5c6fe703587cd878ca765
Mark Vayngrib <mark@tradle.io>
2018-10-04 22:09:14 -0400

    fix: make local lambda testing more convenient

--- src/lambda.ts
+++ src/lambda.ts
@@ -664,7 +664,7 @@ Previous exit stack: ${this.lastExitStack}`)
       const promise = this.preProcess({ event, context })
         .then(() => this.run())
 
-      if (context.done) {
+      if (context && context.done) {
         // until issue is resolved, avoid returning a promise:
         // https://github.com/aws/aws-xray-sdk-node/issues/27#issuecomment-380092859
         promise.then(result => context.done(null, result), context.done)
