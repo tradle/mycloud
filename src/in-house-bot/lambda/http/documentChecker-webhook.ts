2a5b6f77f22751eea97a57071cc4b93750f5ec42
pgmemk <ellen@tradle.io>
2018-10-08 20:07:52 -0400

    feat: Keesing WebAPI project

---
+++ src/in-house-bot/lambda/http/documentChecker-webhook.ts
@@ -0,0 +1,34 @@
+import compose from 'koa-compose'
+import cors from 'kcors'
+import { configureLambda } from '../..'
+import { post } from '../../../middleware/noop-route'
+import Errors from '../../../errors'
+import * as LambdaEvents from '../../lambda-events'
+import { fromHTTP } from '../../lambda'
+
+const lambda = fromHTTP({
+  event: LambdaEvents.DOCUMENT_CHECKER_WEBHOOK_EVENT,
+  preware: compose([
+    post(),
+    cors(),
+  ])
+})
+
+lambda.use(async (ctx) => {
+  debugger
+  const { documentChecker } = ctx.components
+  if (!documentChecker) {
+    throw new Errors.HttpError(404, 'not found')
+  }
+
+  const { event } = ctx
+  try {
+    await documentChecker.handleVerificationEvent(event)
+  } catch (err) {
+    lambda.logger.error('failed to handle documentChecker webhook call', err)
+    ctx.status = 500
+    ctx.error = new Error('failed')
+  }
+})
+
+export const handler = lambda.handler
