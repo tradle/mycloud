37bbc272b711b9859ef293e57b703a520320daed
Mark Vayngrib <mvayngrib@gmail.com>
2018-10-21 10:16:57 -0400

    refactor: increase eth min confirmations 12 -> 20

--- src/networks.ts
+++ src/networks.ts
@@ -21,19 +21,19 @@ const constants:BlockchainNetworkInfo[] = [
     blockchain: 'ethereum',
     networkName: 'mainnet',
     minBalance: '10000000000000000',
-    confirmations: 12
+    confirmations: 20
   },
   {
     blockchain: 'ethereum',
     networkName: 'ropsten',
     minBalance: '10000000000000000',
-    confirmations: 12
+    confirmations: 20
   },
   {
     blockchain: 'ethereum',
     networkName: 'rinkeby',
     minBalance: '10000000000000000',
-    confirmations: 12
+    confirmations: 20
   },
   {
     blockchain: 'corda',
