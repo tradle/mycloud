d00afff0266c1b03f9669b572c612e4510e12714
pgmemk <ellen@tradle.io>
2019-01-06 20:20:10 -0500

    fix: short url does not have scheme

--- src/in-house-bot/middleware/short-to-long-url-mapping.ts
+++ src/in-house-bot/middleware/short-to-long-url-mapping.ts
@@ -17,10 +17,12 @@ export const createMiddleware = (lambda:Lambda, opts:any={}) => {
   return async (ctx:IPBHttpMiddlewareContext, next) => {
     debugger
     const { path, headers } = ctx.event
-    const url = headers.Host + path
+    const url = `${headers['X-Forwarded-Proto']}://${headers.Host}${path}`
+    const { bot, logger } = lambda
+    logger.debug('requesting the mapping resource')
     var items
     try {
-      items = await lambda.bot.db.find({
+      items = await bot.db.find({
         filter: {
           EQ: {
            [TYPE]: SHORT_TO_LONG_URL_MAPPING,
@@ -29,20 +31,21 @@ export const createMiddleware = (lambda:Lambda, opts:any={}) => {
         }
       })
     } catch (err) {
-
+      logger.debug('requesting the mapping resource', err)
     }
 
     if (!items  ||  !items.items.length) {
       // ctx.status = 301
       // ctx.redirect('http://tradle.io')
+      logger.debug(`failed to handle shortToLongURLMapping call: not found resource with short url: ${url}`)
 
       ctx.body = `failed to handle shortToLongURLMapping call: not found resource with short url: ${url}`
       ctx.status = 500
-      ctx.error = new Error('failed')
+      // ctx.error = new Error('failed')
       return
     }
     const r = items.items[0]
+    logger.debug(`redirect to ${r.longUrl}`)
     ctx.redirect(r.longUrl)
-    // ctx.status = 301
   }
 }
