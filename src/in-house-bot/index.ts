14ad0bcac33f837a972c61a35760c9a7212ceca6
pgmemk <ellen@tradle.io>
2019-05-13 15:43:05 -0400

    feat: plugin allows to create more then one application of the same type

--- src/in-house-bot/index.ts
+++ src/in-house-bot/index.ts
@@ -193,11 +193,11 @@ export const loadComponentsAndPlugins = ({
   conf,
   event = ''
 }: {
-    bot: Bot
-    logger: Logger
-    conf: IConfComponents
-    event?: string
-  }): IBotComponents => {
+  bot: Bot
+  logger: Logger
+  conf: IConfComponents
+  event?: string
+}): IBotComponents => {
   const {
     enabled,
     maximumApplications,
@@ -264,11 +264,11 @@ export const loadComponentsAndPlugins = ({
     requiresConf = true,
     prepend
   }: {
-      name: string
-      componentName?: string
-      requiresConf?: boolean
-      prepend?: boolean
-    }) => {
+    name: string
+    componentName?: string
+    requiresConf?: boolean
+    prepend?: boolean
+  }) => {
     const pConf = getPluginConf(name)
     if (requiresConf) {
       const hasConf = !!pConf
@@ -283,7 +283,7 @@ export const loadComponentsAndPlugins = ({
     let api
     let plugin
     try {
-      ; ({ api, plugin } = Plugins.get(name).createPlugin(components, {
+      ;({ api, plugin } = Plugins.get(name).createPlugin(components, {
         conf: pConf,
         logger: logger.sub(`plugin-${name}`)
       }))
@@ -673,12 +673,9 @@ export const loadComponentsAndPlugins = ({
       'facetecZoom',
       'finastra'
     ].forEach(name => attachPlugin({ name }))
-
-      ;[
-        'hand-sig',
-        'documentValidity',
-        'fill-myproduct'
-      ].forEach(name => attachPlugin({ name, requiresConf: false }))
+    ;['hand-sig', 'documentValidity', 'fill-myproduct'].forEach(name =>
+      attachPlugin({ name, requiresConf: false })
+    )
 
     // used for some demo
     // ;[
@@ -730,6 +727,10 @@ export const loadComponentsAndPlugins = ({
     attachPlugin({ name: 'controllingPersonRegistration', componentName: 'smsBasedVerifier' })
   }
   attachPlugin({ name: 'conditional-auto-approve' })
+  if (plugins['limit-applications']) {
+    productsAPI.removeDefaultHandler('onRequestForExistingProduct')
+    attachPlugin({ name: 'limit-applications' })
+  }
 
   logger.debug('using plugins', usedPlugins)
   logger.debug('ignoring plugins', ignoredPlugins)
@@ -756,7 +757,7 @@ const limitApplications = (components: IBotComponents) => {
       await productsAPI.addApplication({ req })
       return
     }
-
+    debugger
     const model = bot.models[type]
     await productsAPI.send({
       req,
---
+++ src/in-house-bot/plugins/limit-applications.ts
@@ -0,0 +1,73 @@
+import _ from 'lodash'
+import validateResource from '@tradle/validate-resource'
+import { TYPE } from '@tradle/constants'
+import { Conf } from '../configure'
+import {
+  getNameFromForm,
+  getDateOfBirthFromForm,
+  getCountryFromForm,
+  getParsedFormStubs,
+  getNonPendingApplications
+} from '../utils'
+import { Bot, CreatePlugin, IPluginLifecycleMethods, ValidatePluginConf } from '../types'
+
+const FORM_REQUEST = 'tradle.FormRequest'
+const PRODUCT_REQUEST = 'tradle.ProductRequest'
+
+interface LimitApplicationsConf {
+  products: {
+    productId: number
+  }
+}
+
+export const createPlugin: CreatePlugin<void> = ({ bot, productsAPI }, { conf, logger }) => {
+  const plugin: IPluginLifecycleMethods = {
+    onRequestForExistingProduct: async req => {
+      const { user, payload } = req
+      debugger
+      if (payload[TYPE] !== PRODUCT_REQUEST) return
+
+      const type = payload.requestFor
+
+      if (!conf.products || !conf.products[type]) return
+
+      const max = conf.products[type] || 1
+      const existing = getNonPendingApplications(user).filter(
+        ({ requestFor }) => requestFor === type
+      )
+      if (existing.length < max) {
+        try {
+          return await productsAPI.addApplication({ req })
+        } catch (err) {
+          logger.error('failed to limitApplications form', {
+            stack: err.stack
+          })
+        }
+      } else {
+        debugger
+        const model = bot.models[type]
+        await productsAPI.send({
+          req,
+          user,
+          object: `You already have a ${model.title}!`
+        })
+      }
+    }
+  }
+
+  return { plugin }
+}
+
+export const validateConf: ValidatePluginConf = async ({ bot, conf, pluginConf }) => {
+  const { models } = bot
+  const { products } = pluginConf
+  if (!products) throw new Error(`empty conf`)
+
+  for (let productId in products) {
+    const max = products[productId]
+    if (!models[productId]) throw new Error(`Unknown product ${productId}`)
+    else if (typeof max !== 'number')
+      throw new Error(`Max number of applications should be a number`)
+    else if (max <= 0) throw new Error(`Max number of applications should be bigger than 0`)
+  }
+}
