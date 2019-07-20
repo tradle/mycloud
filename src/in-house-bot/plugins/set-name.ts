7d726639bbd8a3b8a296794e8a10a131386e8d0b
pgmemk <ellen@tradle.io>
2019-07-20 14:33:06 -0400

    fix: applicant name gets assigned for the application

--- src/in-house-bot/plugins/set-name.ts
+++ src/in-house-bot/plugins/set-name.ts
@@ -8,29 +8,27 @@ const { parseStub } = validateResource.utils
 const PRODUCT_REQUEST = 'tradle.ProductRequest'
 
 export const name = 'setName'
-export const createPlugin = ({ bot, productsAPI }: {
-  bot: Bot
-  productsAPI: any
-}) => {
+export const createPlugin = ({ bot, productsAPI }: { bot: Bot; productsAPI: any }) => {
   const logger = bot.logger.sub('plugin-set-name')
   const productToNameForm = {
     'nl.tradle.DigitalPassport': 'tradle.PhotoID',
     'tradle.onfido.CustomerVerification': 'tradle.PhotoID',
     'tradle.pg.CustomerOnboarding': 'tradle.PhotoID',
+    'tradle.EmployeeOnboarding': 'tradle.PhotoID',
     'tradle.CurrentAccount': 'tradle.PersonalInfo',
     'tradle.LifeInsurance': 'tradle.PersonalInfo',
     'tradle.MortgageProduct': 'tradle.PersonalInfo',
-    'tradle.EmployeeOnboarding': 'tradle.Name',
     'tradle.CordaKYC': 'tradle.BusinessInformation',
     'tradle.CorporateBankAccount': 'tradle.W8BENE1',
-    'tradle.cloud.Deployment': 'tradle.cloud.Configuration',
+    'tradle.cloud.Deployment': 'tradle.cloud.Configuration'
   }
 
-  const trySetName = async (req) => {
+  const trySetName = async req => {
     const { application } = req
     if (!application) return
     if (application.applicantName) return
 
+    // if (!bot.isLocal) return
     try {
       const name = await getName(req)
       if (name) {
@@ -41,29 +39,40 @@ export const createPlugin = ({ bot, productsAPI }: {
     }
   }
 
-  const getName = async (req) => {
+  const getName = async req => {
     const { user, type, payload, application } = req
     if (user.friend) {
       const { name } = await bot.getResource(user.friend)
       return name
     }
-
     if (!(payload && application)) return
     if (type === PRODUCT_REQUEST) return
 
-    const { requestFor, applicantName, forms=[] } = application
+    const { requestFor, applicantName, forms = [] } = application
 
     // if (applicantName) {
     //   logger.debug('applicantName is already set, bye')
     //   return
     // }
 
-    const nameFormType = productToNameForm[requestFor]
+    let nameFormType = productToNameForm[requestFor]
     if (!nameFormType) {
-      const { friend } = user
-      if (!friend) return
+      // const { friend } = user
+      // if (friend) return (await bot.getResource(friend)).name
 
-      return (await bot.getResource(friend)).name
+      if (type !== 'tradle.PhotoID') {
+        if (user.name) return user.name
+        return
+      }
+
+      // const appStubs = user.applications
+      // const employeeApp = appStubs.find(stub => stub.requestFor === 'tradle.EmployeeOnboarding')
+      // // if (employeeApp)
+
+      const model = bot.models[requestFor]
+      const { multiEntryForms } = model
+      if (multiEntryForms && multiEntryForms.includes(type)) return
+      nameFormType = type
     }
 
     let form
@@ -77,7 +86,10 @@ export const createPlugin = ({ bot, productsAPI }: {
     }
 
     let name = getFormattedNameFromForm(form)
-    if (name) return name
+    if (name) {
+      user.name = name
+      return name
+    }
 
     try {
       return buildResource.title({
