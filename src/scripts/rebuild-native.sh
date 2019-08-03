d60c3bc05db8e170fc55c3695cc004be738dc172
pgmemk <ellen@tradle.io>
2019-08-02 21:51:15 -0400

    revert to before canvas commit

--- src/scripts/rebuild-native.sh
+++ src/scripts/rebuild-native.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 # native_modules=$($(dirname $0)/list-native-modules.sh)
-native_modules="secp256k1 sha3 keccak scrypt fsevents segfault-handler canvas"
+native_modules="secp256k1 sha3 keccak scrypt fsevents segfault-handler"
 npm rebuild $native_modules
