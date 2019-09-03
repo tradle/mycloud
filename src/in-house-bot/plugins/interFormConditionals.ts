ea33225faabcbecd000e2aef93d6a14db915118c
pgmemk <ellen@tradle.io>
2019-09-02 22:52:01 -0400

    feat: inter-forms conditionals for props

--- src/in-house-bot/plugins/interFormConditionals.ts
+++ src/in-house-bot/plugins/interFormConditionals.ts
@@ -30,15 +30,15 @@ export const createPlugin: CreatePlugin<void> = ({ bot }, { conf, logger }) => {
       application.forms
         .map(appSub => parseStub(appSub.submission))
         .forEach(f => {
-          if (f.type !== PRODUCT_REQUEST  &&  f.type !== FORM_REQUEST)
+          if (f.type !== PRODUCT_REQUEST && f.type !== FORM_REQUEST)
             promises.push(bot.objects.get(f.link))
         })
 
       const forms = {}
       if (promises.length) {
         try {
-          let result =  await Promise.all(promises)
-          result.forEach(r => forms[r[TYPE]] = r)
+          let result = await Promise.all(promises)
+          result.forEach(r => (forms[r[TYPE]] = r))
         } catch (err) {
           logger.error('interFormConditionals', err)
         }
@@ -55,19 +55,128 @@ export const createPlugin: CreatePlugin<void> = ({ bot }, { conf, logger }) => {
           return
         }
         let val = f[formId]
-        let isAdd = val.startsWith('add: ')
-        val = val.slice(5).trim()
-        try {
-          let ret = new Function('forms', `return ${val}`)(forms)
-          if (ret) {
-            if (isAdd) retForms.push(formId)
-          } else if (!isAdd) retForms.push(formId)
-        } catch (err) {}
+        let isArray
+        if (typeof val === 'string') {
+          if (val.startsWith('set:')) {
+            retForms.push(formId)
+            return
+          }
+          val = [val]
+        }
+        let hasAction
+        val.forEach(v => {
+          let isAdd = val.startsWith('add: ')
+          if (!isAdd && val.startsWith('set: ')) return
+          hasAction = true
+          val = val.slice(5).trim()
+          try {
+            let ret = new Function('forms', `return ${val}`)(forms)
+            if (ret) {
+              if (isAdd) {
+                retForms.push(formId)
+                return
+              }
+            } else if (!isAdd) {
+              retForms.push(formId)
+              return
+            }
+          } catch (err) {
+            debugger
+          }
+        })
+        if (!hasAction) retForms.push(formId)
       })
       return retForms
+    },
+    async willRequestForm({ application, formRequest }) {
+      // debugger
+      if (!application) return
+      const requestFor = application.requestFor
+      let productConf = conf[requestFor]
+      if (!productConf) return
+
+      let ftype = formRequest.form
+      let model = bot.models[ftype]
+      if (!model) return
+      let formConditions = productConf.find(f => typeof f !== 'string' && f[ftype])
+
+      if (!formConditions) return
+
+      formConditions = formConditions[ftype]
+      let setConditions
+      if (Array.isArray(formConditions))
+        setConditions = formConditions.filter(f => f.startsWith('set:'))
+      else setConditions = formConditions.startsWith('set:') && [formConditions]
+
+      if (!setConditions || !setConditions.length) return
+
+      let allForms = []
+      let allFormulas = []
+      setConditions.forEach(async val => {
+        let [propName, formula] = val
+          .slice(5)
+          .split('=')
+          .map(s => s.trim())
+
+        if (!model.properties[propName]) {
+          debugger
+          return
+        }
+
+        let formIds = getForms(formula)
+        if (!formIds.length) return
+
+        allFormulas.push([propName, formula])
+        formIds.forEach(f => !allForms.includes(f) && allForms.push(f))
+      })
+
+      let promises = []
+      application.forms
+        .map(appSub => parseStub(appSub.submission))
+        .forEach(f => {
+          if (allForms.includes(f.type)) promises.push(bot.objects.get(f.link))
+        })
+
+      let forms = {}
+      try {
+        let result = await Promise.all(promises)
+        result.forEach(r => (forms[r[TYPE]] = r))
+      } catch (err) {
+        logger.error('interFormConditionals', err)
+      }
+
+      allFormulas.forEach(async val => {
+        let [propName, formula] = val
+        try {
+          let value = new Function('forms', `return ${formula}`)(forms)
+          formRequest.prefill = {
+            [TYPE]: ftype,
+            [propName]: value
+          }
+        } catch (err) {
+          debugger
+        }
+      })
     }
   }
   return {
     plugin
   }
+}
+function getForms(formula) {
+  let forms = []
+  let idx = 0
+  let len = formula.length
+  while (idx < len) {
+    let idx1 = formula.indexOf("forms['", idx)
+    if (idx1 === -1) break
+    let idx2 = formula.indexOf("']", idx1)
+    if (idx2 === -1) {
+      debugger
+      break
+    }
+    forms.push(formula.slice(idx1 + 7, idx2))
+    idx = idx2 + 2
+  }
+  return forms
 }
