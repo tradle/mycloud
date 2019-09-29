34c273b68fd99b89d213681756f282b4ecde048d
pgmemk <ellen@tradle.io>
2019-09-29 16:02:01 -0400

    feat: risk calc project

---
+++ src/in-house-bot/plugins/riskScore.ts
@@ -0,0 +1,161 @@
+import _ from 'lodash'
+// import validateResource from '@tradle/validate-resource'
+import { TYPE } from '@tradle/constants'
+import { buildResourceStub } from '@tradle/build-resource'
+import {
+  Bot,
+  CreatePlugin,
+  IWillJudgeAppArg,
+  IPBReq,
+  IPluginLifecycleMethods,
+  ValidatePluginConf,
+  ITradleObject,
+  IPBApp,
+  Applications,
+  Logger
+} from '../types'
+
+import { getLatestForms } from '../utils'
+const riskFactors = require('../../../riskFactors.json')
+const CP_ONBOARDING = 'tradle.legal.ControllingPersonOnboarding'
+const CE_ONBOARDING = 'tradle.legal.LegalEntityProduct'
+const CORPORATION_EXISTS = 'tradle.CorporationExistsCheck'
+const BENEFICIAL_OWNER_CHECK = 'tradle.BeneficialOwnerCheck'
+const INITIAL_SCORE = 1000
+const PHOTO_ID = 'tradle.PhotoID'
+const LEGAL_ENTITY = 'tradle.legal.LegalEntity'
+
+export const createPlugin: CreatePlugin<void> = ({ bot, applications }, { conf, logger }) => {
+  const plugin = {
+    async onFormsCollected({ req }) {
+      let { application } = req
+      const stubs = getLatestForms(application)
+      let { requestFor } = application
+      let { weights, countries, legalStructure, industries, lengthOfRelationship } = riskFactors
+      let stub
+      if (requestFor === CP_ONBOARDING) stub = stubs.find(({ type }) => type === PHOTO_ID)
+      else if (requestFor === CE_ONBOARDING) stub = stubs.find(({ type }) => type === LEGAL_ENTITY)
+
+      let resource = await bot.getResource(stub)
+      let coef = countries[resource.country.id.split('_')[1]]
+      let initialValue = INITIAL_SCORE
+      let score = initialValue
+
+      if (coef) {
+        let weight = weights.countries
+        score -= initialValue * weight * coef
+        if (requestFor === CP_ONBOARDING) return
+      }
+      let checks = application.checks.filter(
+        check => check[TYPE] === CORPORATION_EXISTS || check[TYPE] === BENEFICIAL_OWNER_CHECK
+      )
+      if (!checks) {
+        application.score = Math.round(score)
+        this.checkParent(application)
+        return
+      }
+
+      checks = await Promise.all(checks.map(check => bot.getResource(check)))
+      checks.sort((a, b) => b._time - a._time)
+      checks = _.uniqBy(checks, TYPE)
+      let corpExistsCheck = checks.find(
+        check => check[TYPE] === CORPORATION_EXISTS && check.status.id.endsWith('_pass')
+      )
+      if (corpExistsCheck)
+        score = checkOfficers({ score, corpExistsCheck })
+
+      let beneRiskCheck = checks.find(
+        check => check[TYPE] === BENEFICIAL_OWNER_CHECK && check.status.id.endsWith('_pass')
+      )
+      if (!beneRiskCheck || !beneRiskCheck.rawData || !beneRiskCheck.rawData.length) {
+        application.score = Math.round(score)
+        this.checkParent(application)
+        return
+      }
+      let { rawData } = beneRiskCheck
+      rawData.forEach(elm => {
+        let { data } = elm
+        if (!data) return
+        let { kind, identification, address, natures_of_control, ceased_on } = data
+        if (ceased_on) return
+        if (natures_of_control && !natures_of_control.length) {
+          // no control
+          return
+        }
+        let isIndividual = kind.startsWith('individual-')
+        if (isIndividual) {
+        } else {
+          let legalForm = identification.legal_form
+          if (legalForm) {
+            let coef = legalStructure[legalForm.toLowerCase().replace(/\s/g, '_')]
+            if (coef) {
+              score -= initialValue * weights.legalStructure * coef
+            }
+          }
+        }
+      })
+      application.score = Math.round(score)
+      this.checkParent(application, score)
+    },
+    async checkParent(application, score) {
+      application.score = Math.round(score)
+      if (score === INITIAL_SCORE || !application.parent) return
+      let parentApp = await bot.getResource(application.parent)
+      let pscore = parentApp.score
+      if (!pscore || pscore > application.score) {
+        parentApp.score = application.score
+        applications.updateApplication(parentApp)
+      }
+    }
+  }
+  return { plugin }
+}
+
+function checkOfficers({ score, corpExistsCheck }) {
+  let { industry_codes, officers } = corpExistsCheck.rawData[0].company
+
+  let { weights, industries, lengthOfRelationship } = riskFactors
+
+  let initialValue = INITIAL_SCORE
+  if (industry_codes && industry_codes.length) {
+    let ic = industry_codes.filter(
+      code =>
+        code.industry_code.code_scheme_id.startsWith('isic_') &&
+        industries[code.industry_code.code + '']
+    )
+    if (ic.length) {
+      let weight = weights.industry
+      let industryCoef = 0
+      ic.forEach((item: any) => {
+        let coef = industries[item.industry_code.code + '']
+        if (coef && coef > industryCoef) industryCoef = coef
+      })
+      score -= initialValue * weight * industryCoef
+    }
+  }
+  if (officers && officers.length) {
+    if (officers.length)
+      officers = officers.filter(o => o.officer.position !== 'agent' && !o.officer.inactive)
+    let weight = weights.lengthOfRelationship
+    let part = (score * weight) / officers.length
+    let newScore = score - score * weight
+    officers.forEach(o => {
+      let startDate = o.officer.start_date
+      if (!startDate) newScore += part
+      else {
+        let endDate = o.officer.end_date
+        if (endDate) endDate = new Date().getTime()
+        else endDate = Date.now()
+        let delta = endDate - new Date(startDate).getTime()
+        let day = 1000 * 60 * 60 * 24
+        let years = Math.round(delta / day / 365)
+        if (years < 1) years = 1
+        let coef = lengthOfRelationship[years + '']
+        if (coef) newScore += part * coef
+        else newScore += part
+      }
+    })
+    score = newScore
+  }
+  return score
+}
