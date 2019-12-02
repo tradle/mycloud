e44b6d2f4a61a99f6d15f0d90ea847b4503f5761
pgmemk <ellen@tradle.io>
2019-12-02 12:02:25 -0500

    feat: switching to node 10

--- src/scripts/rebuild-lambda.sh
+++ src/scripts/rebuild-lambda.sh
@@ -4,4 +4,4 @@ echo 'rebuilding native modules for lambda environment'
 docker run --rm -v "$PWD:/var/task" \
   --entrypoint "./src/scripts/rebuild-native.sh" \
   -e TRADLE_BUILD="1" \
-  lambci/lambda:build-nodejs8.10
+  lambci/lambda:build-nodejs10.x
