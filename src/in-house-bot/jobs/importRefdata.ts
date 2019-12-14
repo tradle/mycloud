9cf978632828270c1ff245300452caf18e590c01
jacobgins <jacob.gins@tradle.io>
2019-12-14 17:48:43 -0500

    check add dataSource ref

--- src/in-house-bot/jobs/importRefdata.ts
+++ src/in-house-bot/jobs/importRefdata.ts
@@ -1,4 +1,6 @@
+import fs from 'fs'
 import zlib from 'zlib'
+import crypto from 'crypto'
 import fetch from 'node-fetch'
 import AWS from 'aws-sdk'
 
@@ -7,14 +9,22 @@ import {
   Logger,
 } from '../types'
 
+import { TYPE } from '@tradle/constants'
+import { enumValue } from '@tradle/build-resource'
+
 const accessKeyId = ''
 const secretAccessKey = ''
 
 //const BUCKET = 'jacob.gins.athena'
 //const ATHENA_DB = 'sampledb'
 
+const TEMP = '/tmp/' // use lambda temp dir
+
 const GB_PREFIX = 'refdata/gb/'
 const DE_PREFIX = 'refdata/de/'
+
+const REFERENCE_DATA_SOURCES = 'tradle.ReferenceDataSources'
+const DATA_SOURCE_REFRESH = 'tradle.DataSourceRefresh'
 
 const s3 = new AWS.S3({ accessKeyId, secretAccessKey });
 
@@ -31,14 +41,15 @@ export class ImportRefdata {
   }
 
   move = async () => {
-    await this.moveBafin()
-    await this.moveUKFile('EMDAgents', 'emd_agents')
-    await this.moveUKFile('EMoneyFirms', 'e_money_firms')
-    await this.moveUKFile('CreditInstitutions', 'credit_institutions')
-    await this.moveUKFile('PSDFirms', 'firms_psd_perm')
+    let current: Array<string> = await this.list()
+    await this.moveBafin(current)
+    await this.moveUKFile('EMDAgents', 'emd_agents', current)
+    await this.moveUKFile('EMoneyFirms', 'e_money_firms', current)
+    await this.moveUKFile('CreditInstitutions', 'credit_institutions', current)
+    await this.moveUKFile('PSDFirms', 'firms_psd_perm', current)
   }
 
-  moveBafin = async () => {
+  moveBafin = async (current: Array<string>) => {
     var response = await fetch('https://portal.mvp.bafin.de/database/InstInfo/sucheForm.do', {
       method: 'post',
       body: 'sucheButtonInstitut=Search',
@@ -53,30 +64,134 @@ export class ImportRefdata {
     let u = html.substring(qidx, csvidx).replace(/&amp;/g, '&');
 
     let get = await fetch('https://portal.mvp.bafin.de/database/InstInfo/sucheForm.do' + u)
+    let name = `${DE_PREFIX}export.csv.gz`
+    let file = TEMP + name
 
-    let contentToPost = {
+    let fout = fs.createWriteStream(file)
+    let promise = this.writeStreamToPromise(fout)
+    get.body.pipe(zlib.createGzip()).pipe(fout)
+    await promise
+    let md5: string = await this.checksumFile('MD5', file)
+
+    if (current.includes(name)) {
+      // check md5
+      let hash = await this.currentMD5(name)
+      if (md5 == hash) {
+        fs.unlinkSync(file)
+        this.logger.debug(`do not import Bafin data, no change`)
+        return
+      }
+    }
+
+    let rstream = fs.createReadStream(file)
+
+    const contentToPost: AWS.S3.Types.PutObjectRequest = {
       Bucket: this.outputLocation,
-      Key: `${DE_PREFIX}export.csv.gz`,
-      Body: get.body.pipe(zlib.createGzip())
+      Key: name,
+      Metadata: { md5 },
+      Body: rstream
     }
 
     let res = await s3.upload(contentToPost).promise()
+    await this.createDataSourceRefresh('bafin')
     this.logger.debug('imported BaFIN data')
+    fs.unlinkSync(file)
   }
 
-  moveUKFile = async (name: string, table: string) => {
+  moveUKFile = async (name: string, table: string, current: Array<string>) => {
     let get = await fetch(`https://register.fca.org.uk/ShPo_registerdownload?file=${name}`)
     let html = await get.text()
     let idx1 = html.indexOf('.handleRedirect(')
     let idx2 = html.indexOf('\'', idx1 + 18)
     get = await fetch('https://register.fca.org.uk' + html.substring(idx1 + 17, idx2))
 
+    let key = `${GB_PREFIX}${table}/${name}.csv.gz`
+    let file = TEMP + key
+
+    let fout: fs.WriteStream = fs.createWriteStream(file)
+    let promise = this.writeStreamToPromise(fout)
+    get.body.pipe(zlib.createGzip()).pipe(fout)
+    await promise
+    let md5: string = await this.checksumFile('MD5', file)
+
+    if (current.includes(key)) {
+      // check md5
+      let hash = await this.currentMD5(key)
+      if (md5 == hash) {
+        fs.unlinkSync(file)
+        this.logger.debug(`do not import ${name} data, no change`)
+        return
+      }
+    }
+
+    let rstream: fs.ReadStream = fs.createReadStream(file)
+
     let contentToPost = {
       Bucket: this.outputLocation,
-      Key: `${GB_PREFIX}${table}/${name}.csv.gz`,
-      Body: get.body.pipe(zlib.createGzip())
+      Key: key,
+      Metadata: { md5 },
+      Body: rstream
     }
     let res = await s3.upload(contentToPost).promise()
+
+    await this.createDataSourceRefresh(`fca.${name}`)
+
     this.logger.debug(`imported ${name} data`)
+    fs.unlinkSync(file)
   }
+
+  checksumFile = (algorithm: string, path: string): Promise<string> => {
+    return new Promise((resolve, reject) =>
+      fs.createReadStream(path)
+        .on('error', reject)
+        .pipe(crypto.createHash(algorithm)
+          .setEncoding('hex'))
+        .once('finish', function () {
+          resolve(this.read())
+        })
+    )
+  }
+
+  list = async (): Promise<Array<string>> => {
+    let params = {
+      Bucket: this.outputLocation,
+      Prefix: 'refdata/'
+    }
+    let keys = []
+    let data = await s3.listObjectsV2(params).promise()
+
+    for (let content of data.Contents) {
+      keys.push(content.Key)
+    }
+    return keys
+  }
+
+  currentMD5 = async (key: string) => {
+    var params = {
+      Bucket: this.outputLocation,
+      Key: key
+    }
+    let resp = await s3.headObject(params).promise()
+    return resp.Metadata.md5
+  }
+
+  createDataSourceRefresh = async (name: string) => {
+    let provider = enumValue({
+      model: this.bot.models[REFERENCE_DATA_SOURCES],
+      value: name
+    })
+    let resource = {
+      [TYPE]: DATA_SOURCE_REFRESH,
+      name: provider,
+      timestamp: Date.now()
+    }
+    await this.bot.signAndSave(resource)
+  }
+
+  writeStreamToPromise = (stream: fs.WriteStream) => {
+    return new Promise((resolve, reject) => {
+      stream.on('finish', resolve).on('error', reject)
+    })
+  }
+
 }
