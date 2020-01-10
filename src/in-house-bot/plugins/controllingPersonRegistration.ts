3220db265d14144a0f276fe7f553e82a7d62b3be
pgmemk <ellen@tradle.io>
2020-01-10 16:45:56 -0500

    refactor: code cleanup

--- src/in-house-bot/plugins/controllingPersonRegistration.ts
+++ src/in-house-bot/plugins/controllingPersonRegistration.ts
@@ -821,31 +821,3 @@ export const validateConf: ValidatePluginConf = async ({
       '"messages" in the rules should be an array. Index of the message in array signifies what message will be send depending on what time the message is getting send out'
     )
 }
-// async onResourceCreated(payload) {
-//   if (payload[TYPE] === CONTROLLING_PERSON) {
-//     if (getEnumValueId(payload.typeOfControllingEntity) === 'person') return
-//     let {
-//       name,
-//       emailAddress,
-//       controllingEntityCompanyNumber,
-//       controllingEntityCountry,
-//       controllingEntityRegion,
-//       controllingEntityPostalCode,
-//       controllingEntityStreetAddress
-//     } = payload
-//     let le = sanitize({
-//       [TYPE]: 'tradle.legal.LegalEntity',
-//       companyName: name,
-//       registrationNumber: controllingEntityCompanyNumber,
-//       country: controllingEntityCountry,
-//       region: controllingEntityRegion,
-//       emailAddress,
-//       streetAddress: controllingEntityStreetAddress,
-//       postalCode: controllingEntityPostalCode
-//     }).sanitized
-//     let legalEntity = await bot
-//       .draft({ [TYPE]: 'tradle.legal.LegalEntity' })
-//       .set(le)
-//       .signAndSave()
-//   }
-// },
