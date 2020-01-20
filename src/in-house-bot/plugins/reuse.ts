bbfa5297ec9ca14418978748510ccdd615e60e76
pgmemk <ellen@tradle.io>
2020-01-19 22:21:23 -0500

    feat: reuse previously onboarded entity

---
+++ src/in-house-bot/plugins/reuse.ts
@@ -0,0 +1,43 @@
+import _ from 'lodash'
+import { TYPE } from '@tradle/constants'
+import { CreatePlugin, IPBReq, IPluginLifecycleMethods } from '../types'
+
+const CP = 'tradle.legal.LegalEntityControllingPerson'
+const LEGAL_ENTITY = 'tradle.legal.LegalEntity'
+const REUSE_CHECK = 'tradle.ReuseOfDataCheck'
+const ASPECTS = 'Reusing previously onboarded entity'
+const PROVIDER = 'Tradle'
+
+export const createPlugin: CreatePlugin<void> = ({ bot, applications }, { conf, logger }) => {
+  const plugin: IPluginLifecycleMethods = {
+    async onmessage(req: IPBReq) {
+      let { application, payload } = req
+      if (!application) return
+
+      if (payload[TYPE] !== CP || !payload.controllingEntityCompanyNumber) return
+
+      let legalEntity = await bot.db.findOne({
+        filter: {
+          EQ: {
+            [TYPE]: LEGAL_ENTITY,
+            registrationNumber: payload.controllingEntityCompanyNumber
+          }
+        }
+      })
+      if (!legalEntity) return
+      let resource: any = {
+        [TYPE]: REUSE_CHECK,
+        status: 'warning',
+        provider: PROVIDER,
+        application,
+        dateChecked: Date.now(), //rawData.updated_at ? new Date(rawData.updated_at).getTime() : new Date().getTime(),
+        aspects: ASPECTS,
+        form: payload,
+        message: 'Please overwrite if data can`t be reused'
+      }
+      await applications.createCheck(resource, req)
+    }
+  }
+
+  return { plugin }
+}
