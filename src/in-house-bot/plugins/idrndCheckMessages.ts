9dcd6044510e72a7434c28cf9a68257fe7d9ed88
jacobgins <jacob.gins@tradle.io>
2020-05-28 17:40:33 -0400

    idrndCheck messages

--- src/in-house-bot/plugins/idrndCheckMessages.ts
+++ src/in-house-bot/plugins/idrndCheckMessages.ts
@@ -1,6 +1,7 @@
 export const messages = {
   en: {
     FACE_TOO_CLOSE: "Face is too close to the camera",
+    FACE_CLOSE_TO_BORDER: "Face is too close to one or more borders",
     FACE_NOT_FOUND: "Face is not found",
     FACE_TOO_SMALL: "Face is too far from the camera",
     FACE_ANGLE_TOO_LARGE: "Face the camera directly",
