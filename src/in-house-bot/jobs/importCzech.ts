c96bf77edd4d148f76cd069e6b9308cd7e451481
jacobgins <jacob.gins@tradle.io>
2020-07-30 18:04:28 -0400

    use S3 select instead of athena

--- src/in-house-bot/jobs/importCzech.ts
+++ src/in-house-bot/jobs/importCzech.ts
@@ -17,10 +17,10 @@ import { enumValue } from '@tradle/build-resource'
 
 const CZ_COMPANIES_PREFIX = 'refdata/cz/companies/'
 const CZ_PREFIX_TEMP = 'temp/refdata/cz/companies_origin/'
-const CZ_PREFIX_ORC_TEMP = 'temp/refdata/cz/companies_orc/'
+const CZ_PREFIX_ORC_TEMP = 'temp/refdata/cz/companies_parquet/'
 const TEMP = '/tmp/' // use lambda temp dir
 const TIME_LIMIT = 11 * 60 * 1000
-const BUCKET_COUNT = 4
+const BUCKET_COUNT = 128
 
 const REFERENCE_DATA_SOURCES = 'tradle.ReferenceDataSources'
 const DATA_SOURCE_REFRESH = 'tradle.DataSourceRefresh'
@@ -172,16 +172,16 @@ export class ImportCzechData {
          data string, 
          ceasedate string, 
          recorddate string)
-      CLUSTERED BY ( ico) 
+      CLUSTERED BY (ico) 
       INTO ${BUCKET_COUNT} BUCKETS
       ROW FORMAT SERDE 
-        'org.apache.hadoop.hive.ql.io.orc.OrcSerde' 
+        'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' 
       STORED AS INPUTFORMAT 
-        'org.apache.hadoop.hive.ql.io.orc.OrcInputFormat' 
+        'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat' 
       OUTPUTFORMAT 
-        'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'
+        'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
       LOCATION
-      's3://${this.outputLocation}/${CZ_COMPANIES_PREFIX}'
+        's3://${this.outputLocation}/${CZ_COMPANIES_PREFIX}'
       TBLPROPERTIES ('has_encrypted_data'='false')`
 
     await this.executeDDL(createTab, 2000)
@@ -220,7 +220,7 @@ export class ImportCzechData {
 
     const create = `CREATE TABLE czech_data_origin_bucketed 
                     WITH (
-                    format = 'ORC', 
+                    format = 'PARQUET', 
                     external_location = 's3://${this.outputLocation}/${CZ_PREFIX_ORC_TEMP}', 
                     bucketed_by = ARRAY['ico'], 
                     bucket_count = ${BUCKET_COUNT})
@@ -349,4 +349,5 @@ export class ImportCzechData {
       return undefined
     }
   }
+
 }
