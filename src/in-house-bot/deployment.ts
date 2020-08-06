c1362cf13c2e29c02eaa71dab578ff65cf0d9f33
pgmemk <ellen@tradle.io>
2020-08-06 17:29:31 -0400

    refactor: code cleanup

--- src/in-house-bot/deployment.ts
+++ src/in-house-bot/deployment.ts
@@ -1181,10 +1181,6 @@ ${this.genUsageInstructions(links)}`
     const key = `templates/template-${commit}-${Date.now()}-${randomStringWithLength(10)}.json`
     await this._bucket(bucket, region).putJSON(key, template, { acl: 'public-read' })
 
-    // HACK
-    if (region !== 'me-south-1')
-      region = null
-    //
     const url = this.bot.s3Utils.getUrlForKey({ bucket, key, region })
     this.logger.silly('saved template', { bucket, key, url })
     return url
