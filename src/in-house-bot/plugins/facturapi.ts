480690c14ec2f5716ed5c9d2d6709d34fafa9b5c
pgmemk <ellen@tradle.io>
2021-07-15 14:25:08 -0400

    feat: get rid of hardcoded invoice type

--- src/in-house-bot/plugins/facturapi.ts
+++ src/in-house-bot/plugins/facturapi.ts
@@ -30,6 +30,7 @@ import {
 
 interface IFacturapiConf {
   authorization: string
+  invoiceType: string
   trace?: boolean,
 }
 
@@ -62,21 +63,21 @@ export class FacturAPI {
   
   private post = async (data: any, url: string) => {
     try {
-        let res = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-              Authorization: 'Basic ' + this.encodeStringToBase64(this.conf.authorization + ':'),
-              'Content-Type': 'application/json; charset=utf-8'
-            }
-        })
+      let res = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            Authorization: 'Basic ' + this.encodeStringToBase64(this.conf.authorization + ':'),
+            'Content-Type': 'application/json; charset=utf-8'
+          }
+      })
 
-        if (res.ok) {
-          const result = await res.json();
-          this.logger.debug(result)
-        } else {
-          this.logger.error('status=' + res.status, res.statusText)
-        }
+      if (res.ok) {
+        const result = await res.json();
+        this.logger.debug(result)
+      } else {
+        this.logger.error('status=' + res.status, res.statusText)
+      }
     } catch (err) {
       this.logger.error(err.message)
     }
@@ -98,12 +99,13 @@ export const createPlugin: CreatePlugin<void> = ({ bot, applications }, { conf, 
       const { user, application, payload } = req
       if (!application) return
 
-      if (INVOICE_TYPE !== payload[TYPE])
+      const { invoiceType } = conf 
+      if (invoiceType !== payload[TYPE])
         return
-
       if (!payload[NAME] || ! payload[EMAIL] || !payload[RFC] || 
           !payload[DESCRIPTION] || !payload[PRODUCT_KEY] || !payload[PRICE])  
         return
+debugger
 
       let changed = await hasPropertiesChanged({
         resource: payload,
@@ -148,7 +150,12 @@ export const validateConf: ValidatePluginConf = async ({
   conf: IConfComponents
   pluginConf: IFacturapiConf
 }) => {
-  if (!pluginConf.authorization || typeof pluginConf.authorization !== 'string') {
+  const { invoiceType, authorization } = pluginConf
+  if (!invoiceType)
+    throw new Error(`No 'invoiceType' found in configuration`)
+  if (!bot.models[invoiceType])
+    throw new Error(`Invalid 'invoiceType' ${invoiceType}`)
+  if (!authorization || typeof authorization !== 'string') {
     throw new Errors.InvalidInput(`property 'authorization' is not set`)
   }
 }
