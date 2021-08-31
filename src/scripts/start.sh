a8cd515aae8bcc89c6e2310933c84f6a51edc858
Martin Heidegger <martin.heidegger@gmail.com>
2021-08-31 18:50:45 +0900

    fix: allowing custom server with start script

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
-docker ps >/dev/null 2>&1 || { echo 'please start Docker first'; exit 1; }
+echo "${TRADLE_LOCAL_IP}"
+
+if [[ -z "${TRADLE_LOCAL_IP}" ]]; then
+  docker ps >/dev/null 2>&1 || { echo 'please start Docker first'; exit 1; }
+fi
 
 npm run localstack:start
 npm run fix:redis
