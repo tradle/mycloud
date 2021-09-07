146880b635956abe57fac7a8bde52ee76106da32
Martin Heidegger <martin.heidegger@gmail.com>
2021-09-07 16:54:20 +0900

    updating lambda rebuild script

--- src/scripts/rebuild-native.sh
+++ src/scripts/rebuild-native.sh
@@ -3,6 +3,5 @@
 set -euo pipefail
 
 # native_modules=$($(dirname $0)/list-native-modules.sh)
-native_modules="secp256k1 sha3 keccak scrypt fsevents segfault-handler"
+native_modules="secp256k1 keccak segfault-handler sharp napi-build-utils"
 npm rebuild $native_modules
-npm i sharp
