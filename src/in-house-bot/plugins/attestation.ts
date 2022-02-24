60b05bb5bf820ea55dd5c64c95b122ece87b5658
pgmemk <ellen@tradle.io>
2022-02-23 19:10:19 -0500

    feat: allow attestation be in the subordinate application anywhere in the application

--- src/in-house-bot/plugins/attestation.ts
+++ src/in-house-bot/plugins/attestation.ts
@@ -48,38 +48,92 @@ class AttestationsAPI {
     this.applications = applications
     this.riskFactors = conf.riskFactors
   }
-  async checkAttestation(req) {
-    let { payload, application } = req
-    let rejected = payload.items.filter(item => !item.confirmation)
-    if (!rejected.length) return
+  // async checkAttestation(req) {
+  //   let { payload, application } = req
+  //   let rejected = payload.items.filter(item => !item.confirmation)
+  //   if (!rejected.length) return
 
-    let noApplication = !application
-    if (noApplication) {
-      application = await this.bot.getResource(payload.application, { backlinks: ['forms'] })
-      req.application = application
-    }
+  //   let noApplication = !application
+  //   if (noApplication) {
+  //     application = await this.bot.getResource(payload.application, { backlinks: ['forms'] })
+  //     req.application = application
+  //   }
+  //   let { models } = this.bot
+
+  //   let form = application.forms.find(
+  //     form => form.submission[TYPE] === PHOTO_ID || form.submission[TYPE] === PERSONAL_INFO
+  //   )
+  //   await this.applications.createCheck(
+  //     {
+  //       [TYPE]: ATTESTATION_CHECK,
+  //       status: 'warning',
+  //       provider: application.applicantName,
+  //       application: payload.parentApplication,
+  //       attestedBy: form.submission,
+  //       dateChecked: Date.now(),
+  //       aspects: ASPECTS,
+  //       form: payload,
+  //       message: `Failed confirmation: rejected ${rejected
+  //         .map(r => getDisplayName({ models, model: models[r.item[TYPE]], resource: r.item }))
+  //         .join(', ')}`
+  //     },
+  //     req
+  //   )
+  //   if (noApplication) req.application = null
+  // }
+  async createAndSendAttestation({ application }) {
+    let { parent, associatedResource } = application
+    if (!parent.forms)
+      parent = await this.bot.getResource(parent, { backlinks: ['forms', 'notifications'] })
+  debugger
+    let { forms } = parent
     let { models } = this.bot
+    forms = forms.filter(f => !exclude.includes(f.submission[TYPE])  &&
+                        f.submission[TYPE] !== associatedResource[TYPE] &&
+                        isSubClassOf(FORM, models[f.submission[TYPE]], models))
+    const { properties } = this.bot.models[associatedResource[TYPE]]
+    // HACK for CO
+    if (properties.isSeniorManager) {
+      associatedResource = await this.bot.getLatestResource(associatedResource)
+      if (!associatedResource.isSeniorManager) return
+    }
 
-    let form = application.forms.find(
-      form => form.submission[TYPE] === PHOTO_ID || form.submission[TYPE] === PERSONAL_INFO
-    )
-    await this.applications.createCheck(
-      {
-        [TYPE]: ATTESTATION_CHECK,
-        status: 'warning',
-        provider: application.applicantName,
-        application: payload.parentApplication,
-        attestedBy: form.submission,
-        dateChecked: Date.now(),
-        aspects: ASPECTS,
-        form: payload,
-        message: `Failed confirmation: rejected ${rejected
-          .map(r => getDisplayName({ models, model: models[r.item[TYPE]], resource: r.item }))
-          .join(', ')}`
-      },
-      req
-    )
-    if (noApplication) req.application = null
+    // forms.sort((a, b) => b._time - a._time)
+    forms = forms.map(f => f.submission)
+    forms = _.uniqBy(forms, '_permalink')
+
+    forms = await Promise.all(forms.map(f => this.bot.getResource(f)))
+
+    return {
+      [TYPE]: FORM_REQUEST,
+      form: ATTESTATION,
+      product: parent.requestFor,
+      message: 'Please review and confirm',
+      prefill: {
+        [TYPE]: ATTESTATION,
+        application,
+        parentApplication: parent,
+        items: forms.map(resource => {
+          let props = this.bot.models[resource[TYPE]].properties
+          let r: any = {}
+
+          for (let p in resource) {
+            if (props[p]) r[p] = resource[p]
+          }
+          r[TYPE] = resource[TYPE]
+          // r._link = resource._link
+          r._permalink = resource._permalink
+          delete r.top
+          delete r.parent
+          return {
+            [TYPE]: ATTESTATION_ITEM,
+            // itemPermalink: resource._permalink,
+            // itemLink: resource._link,
+            item: r
+          }
+        })
+      }
+    }
   }
 }
 export const createPlugin: CreatePlugin<void> = ({ bot, applications }, { logger, conf }) => {
@@ -96,7 +150,8 @@ export const createPlugin: CreatePlugin<void> = ({ bot, applications }, { logger
     //   let { application, payload } = req
 
     //   if (payload[TYPE] === ATTESTATION) {
-    //     await attestationAPI.checkAttestation(req)
+    //     // await attestationAPI.checkAttestation(req)
+    //     debugger
     //     return
     //   }
     //   if (!application) return
@@ -109,8 +164,24 @@ export const createPlugin: CreatePlugin<void> = ({ bot, applications }, { logger
 
     //   items = items.filter(item => products.includes(item[TYPE]))
     //   if (!items.length) return
-    //   debugger
+      // debugger
     // },
+    async willRequestForm({ application, formRequest, req }) {
+      if (!application) return
+
+      let { requestFor, parent, associatedResource } = application
+
+      let { products } = conf
+      if (!products || !products.includes(requestFor)) return
+
+      if (!parent || !associatedResource) return
+
+      let hasDataBundle = application.submissions.find(sub => sub.submission[TYPE] === ATTESTATION)
+      if (hasDataBundle) return
+
+      let item = await attestationAPI.createAndSendAttestation({application})
+      _.extend(formRequest, item)
+    },
     async didApproveApplication(opts: IWillJudgeAppArg, certificate: ITradleObject) {
       let { application, user, req } = opts
 
