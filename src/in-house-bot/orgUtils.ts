c4c0182b1aaa9bf6c06a2412360edf7c0bb95333
pgmemk <ellen@tradle.io>
2023-03-28 11:35:58 -0400

    refactor: optimizations

--- src/in-house-bot/orgUtils.ts
+++ src/in-house-bot/orgUtils.ts
@@ -16,15 +16,16 @@ export async function mergeWithDocData({isCompany, isPrefill, application, resou
   bot: Bot,
   aiCheck?: any,
 }) {
+  let emptyReturn = {}
   if (!aiCheck) {
     let { checks } = application
     let check = checks && checks.find(ch => ch[TYPE] === AI_CORPORATION_CHECK)
-    if (!check) return
+    if (!check) return emptyReturn
     aiCheck = check.rawData ? check : await bot.getResource(check)
   }
   let prop = isCompany ? COMPANY_FORMATION_PROP : ARTICLES_PROP
   const val = aiCheck.rawData[prop]
-  if (!val) return
+  if (!val) return emptyReturn
   let newResource = isPrefill ? resource : cloneDeep(resource)
   if (isCompany) {
     extend(newResource, val)
@@ -36,10 +37,10 @@ export async function mergeWithDocData({isCompany, isPrefill, application, resou
   
   let person = val.find(v => {
     let { firstName: f, lastName: l } = v
-    if (!f || !l) return 
+    if (!f || !l) return emptyReturn
     return v.firstName.toLowerCase() === f && v.lastName.toLowerCase() === l
   })
-  if (!person) return
+  if (!person) return emptyReturn
   extend(newResource, person)
   return newResource  
 }
